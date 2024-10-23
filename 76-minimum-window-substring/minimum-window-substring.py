b527a3d13c941162f17b4268002ac1e40d8a8be3
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2024-10-23 08:21:41 -0700

    Time: 63 ms (98.99%) | Memory: 17.2 MB (93.89%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if t == "":
+            return ""
+        have, need ,l = 0,0, 0
+        map1 = {}
+        map2 = {}
+        res = [-1,-1]
+        rlen = float("infinity")
+        for i in t :
+            map1[i]= 1 + map1.get(i,0)
+        need = len(map1)
+        for r in range(len(s)):
+            c = s[r]
+            map2[c] = 1 + map2.get(c,0)
+            #check
+            if c in map1 and map1[c]== map2[c]:
+                have += 1
+            while( have == need):
+                if ( r-l + 1 ) < rlen:
+                    rlen =  r - l + 1
+                    res= [l, r]
+                #pop from left
+                
+                map2[s[l]] -= 1
+                if s[l] in map1 and map1[s[l]] > map2[s[l]]:
+                    have -= 1
+                l += 1
+        l,r = res
+        return s[l:r+1]
+            
+#TC O(N)
+# SC
+ 
+        
