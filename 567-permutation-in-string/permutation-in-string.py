2d633df81418036b5c2752c42e333533450b94c6
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2024-10-24 11:44:43 -0700

    Time: 23 ms (98.25%) | Memory: 16.8 MB (7.64%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,36 +1,4 @@
-# class Solution:
-#     def checkInclusion(self, s1: str, s2: str) -> bool:
-#         #approch 2
-#         map1 = {}
-#         map2 = {}
-#         l,have = 0, 0
-#         for i in s1:
-#             map1[i] = 1 + map1.get(i , 0)
-#         need = len(map1)
-#         r =0
-#         # for r in range(len(s2)):
-#         while r < len(s2):
-#             c = s2[r]
-            
-#             if c in map1 :
-#                 l = r
-#                 while l < len(s2) and  s2[l] in map1 :
-#                     map2[s2[l]] = 1 + map2.get(s2[l],0)
-#                     if map2[s2[l]]== map1[s2[l]]:
-#                         have += 1
-#                     elif map2[s2[l]] > map1[s2[l]]:
-#                         # break
-#                         map2 = {}
-#                         map2[s2[l]] = 1 + map2.get(s2[l],0)
-#                         have = 1
-#                     if have == need:
-#                         return True
-#                     l += 1
-#                 r = l
-#                 map2 = {}
-#                 have = 0
-#             r += 1
-#         return False
+
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         map1, map2 = {}, {}
@@ -66,5 +34,14 @@ class Solution:
         
         return False
                 
-# # tle : as restarting check from r
+# # tle : as restarting check from r , when resetting map2 & have
+# instead use sliding window, l=from left remove , right add
+
+
+# Time Complexity: 
+# O(∣s1∣+∣s2∣)
+
+# Space Complexity: 
+# \U0001d442(1)
+# O(1), because we are only using fixed-size maps map1 and map2 that store the frequencies of the 26 possible lowercase characters.
        
