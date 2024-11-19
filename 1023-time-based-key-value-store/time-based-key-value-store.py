aec6604258c4948244be2a196f8cb9d4714d6660
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2024-11-19 10:19:36 -0800

    Time: 156 ms (22.77%) | Memory: 73.4 MB (78.68%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.py
+++ 1023-time-based-key-value-store/time-based-key-value-store.py
@@ -7,39 +7,33 @@ class TimeMap:
         if key not in self.timeMap:
             self.timeMap[key] = []
         self.timeMap[key].append((timestamp, value))
-        # self.timeMap.get(key, []).append((timestamp, value))
         
 
     def get(self, key: str, timestamp: int) -> str:
-        # print("self.timeMap:: ",self.timeMap)
         res = self.timeMap.get(key, "")
-        # print("resu",res)
         if (res == "" or not res):
             return res
         if(len(res)  == 1 and res[0][0] <= timestamp):
             return res[0][1]
         l = 0
         r = len(res)-1
-        result = ""#(res[0][0], res[0][1])
+        result = ""
         while l <= r:
             m = (l + r) //2
             if( res[m][0] == timestamp ):
-                # result = res[m][1]
                 return res[m][1]
-            # if( res[m][0] < timestamp and res[m][0] > result[0] ):
-            #     result = (res[m][0],res[m][1])
             if timestamp > res[m][0]:
+                #more we move right , more max close value to timestam be assigned
                 result = res[m][1]
                 l = m + 1
-
             else:
-                
                 r = m -1
         return result
             
         
 
-        
+# TC 1) O(1) 2). O(1) 3) Ologn
+# SC      O(n)  
 
 
 # Your TimeMap object will be instantiated and called as such:
