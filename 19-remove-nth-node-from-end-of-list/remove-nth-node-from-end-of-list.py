b83170f492cb2712c2ac72160fa57fb348d90197
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2024-12-04 09:37:14 -0800

    Time: 0 ms (100.00%) | Memory: 17.3 MB (5.15%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -5,24 +5,37 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        slow = dummy
+        fast = head
+        while n > 0 and fast:
+            fast = fast.next
+            n -= 1
+        while fast:
+            slow = slow.next
+            fast = fast.next
+        slow.next = slow.next.next
+        return dummy.next
 
-        d= cur = head
-        l = 0
-        while cur:
-            cur = cur.next
-            l += 1
-        r = l - n
-        if r == 0:
-            return head.next
-        while r != 0:
+        
+        # Approch 2
+#         d= cur = head
+#         l = 0
+#         while cur:
+#             cur = cur.next
+#             l += 1
+#         r = l - n
+#         if r == 0:
+#             return head.next
+#         while r != 0:
             
-            r -=1
-            if( r == 0):
-                d.next = d.next.next
-            else:
-                d = d.next
+#             r -=1
+#             if( r == 0):
+#                 d.next = d.next.next
+#             else:
+#                 d = d.next
 
-        return head
+#         return head
 
-# TC O(N) = O(N) + O(N)
-# SC O(1)    
+# # TC O(N) = O(N) + O(N)
+# # SC O(1)    
