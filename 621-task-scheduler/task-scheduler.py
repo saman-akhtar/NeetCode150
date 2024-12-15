427974e8cf690ed411ef868b209be2be2ad434ea
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2024-12-15 15:19:03 -0800

    Time: 141 ms (43.38%) | Memory: 19 MB (19.82%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -13,9 +13,20 @@ class Solution:
                 cur = heapq.heappop(heap)
                 rem = cur + 1
                 if( rem < 0):
-                    q.append((rem, t + n ))
+                    q.append((rem, t + n))
                     
             if  q and q[0][1] == t:
-                res = q.popleft()
+                res= q.popleft()
                 heapq.heappush(heap,res[0] )
         return t
+# TC  O(T⋅logk), where 
+# \U0001d447
+# T is the total number of tasks (including idle slots), and 
+# \U0001d458
+# k is the number of unique tasks. Each heap operation takes 
+# O(logk).
+# SC O(k) for the heap and 
+
+# O(k) for the cooldown queue, where 
+# \U0001d458
+# k is the number of unique tasks.
