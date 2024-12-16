891c209bb74c7bbb7565b91317c750f561b4bfff
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2024-12-15 19:50:21 -0800

    Time: 5 ms (26.94%) | Memory: 27.2 MB (10.67%) - LeetSync

--- 355-design-twitter/design-twitter.py
+++ 355-design-twitter/design-twitter.py
@@ -13,15 +13,43 @@ class Twitter:
 
     def postTweet(self, userId: int, tweetId: int) -> None:
         self.tweetMap[userId].append((self.time, tweetId))
-        self.time +=1
+        # self.time +=1 #approach 1
+        self.time -=1
         
 
     def getNewsFeed(self, userId: int) -> List[int]:
-        feed =  self.tweetMap[userId][:]
-        for follwee in self.followMap[userId]:
-            feed.extend(  self.tweetMap[follwee])
-        feed.sort(key = lambda x:-x[0])
-        return [tweetId for _,tweetId in feed[:10]]
+        # approach 2
+        self.followMap[userId].add(userId)
+        minHeap = []
+        res = []
+        for followeeId in self.followMap[userId]:
+            # edge case ass check
+            if followeeId in self.tweetMap:
+                index = len(self.tweetMap[followeeId])-1
+                count, tweetId = self.tweetMap[followeeId][index]
+                minHeap.append([count, tweetId,  followeeId, index -1] )
+        heapq.heapify(minHeap)
+        while minHeap and len(res) < 10:
+            count, tweetId, followeeId, index =  heapq.heappop(minHeap)
+            res.append(tweetId)
+            if index >=0:
+                count, tweetId = self.tweetMap[followeeId][index]
+                heapq.heappush(minHeap, [count, tweetId, followeeId, index -1 ])
+        return res
+
+
+
+
+
+        #Approach 1
+    #     feed =  self.tweetMap[userId][:]
+    #     for follwee in self.followMap[userId]:
+    #         feed.extend(  self.tweetMap[follwee])
+    #     feed.sort(key = lambda x:-x[0])
+    #     return [tweetId for _,tweetId in feed[:10]]
+    
+    # # O(NlogN where N=U+T , u = total no of tweet by cur user, Tc = total no of twee of follower)
+#  , the total number of tweets in the feed.
         
 
     def follow(self, followerId: int, followeeId: int) -> None:
