4fa4596503fad622f75ac0521ca37610cf16fa0f
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2024-12-16 08:35:57 -0800

    Time: 0 ms (100.00%) | Memory: 17.4 MB (16.37%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,22 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        # here focu on subproblem
+        # for [1, 2, 3]
+        # if nothig perm is [[]]
+        # for 3 [[3]]
+        # for 2,3 [[2,3],[3,2]]
+
+        path = []
+        if len(nums)==0:
+            return [[]]
+        perms = self.permute(nums[1:])
+        res = []
+        # inssert nums[0]
+        for p in perms:
+            for i in range(len(p)+ 1):
+                p_copy = p.copy()
+                p_copy.insert(i, nums[0])
+                res.append(p_copy)
+        return res
+
+        return res
