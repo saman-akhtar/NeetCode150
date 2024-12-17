95ad9decf0bb4b9c2345ba4ffd5b893f47322980
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2024-12-17 08:30:18 -0800

    Time: 254 ms (9.26%) | Memory: 38.5 MB (22.54%) - LeetSync

---
+++ 494-target-sum/target-sum.py
@@ -0,0 +1,42 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        # # APPROACH 2
+        dp = {}
+
+        def dpfunc(i, total):
+            if i == len(nums):
+                return 1 if total == target else 0
+            if( (i,total ) in dp):
+                return dp[(i,total)]
+            dp[(i,total)] = dpfunc(i +1, total + nums[i]) + dpfunc(i +1, total - nums[i])
+            return dpfunc(i,total)
+        return dpfunc(0,0)
+            
+
+        # # APPROACH 1
+
+
+        # count = 0
+
+        # def backtrack(i, total):
+        #     nonlocal count
+
+        #     # if total == target:
+        #     #     count += 1
+        #     #     return 
+        #     if i >= len(nums):
+        #         if total == target:
+        #             count += 1
+        #         return
+            
+        #     #add
+        #     backtrack(i + 1, total + nums[i])
+
+        #     #subrtact
+        #     backtrack(i + 1, total - nums[i])
+        #     return
+        # backtrack(0, 0)
+        # return count
+
+        # TC 2 ^ n
+        # SC O(N)
