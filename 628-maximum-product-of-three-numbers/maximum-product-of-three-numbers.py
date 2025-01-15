b649aa3b59d87057e31dd1256a672672465c8859
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-01-15 09:15:27 -0800

    Time: 7 ms (89.07%) | Memory: 18.6 MB (36.71%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.py
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.py
@@ -1,14 +1,33 @@
 class Solution:
     def maximumProduct(self, nums: List[int]) -> int:
-        nums.sort()
-        # if len(nums) > 3 and nums[0] < 1 and nums[1] < 1 and ((nums[0] * nums[1]) > (nums[-1] * nums[-2]) or nums[-3] < 1):
+
+        # # Appproach 1
+
+        # # TC O (Nlogn)
+        # # O(1)
+        # nums.sort()
+        # return max( nums[0] * nums[1]* nums[-1], nums[-1] * nums[-2] * nums[-3])
+        max1 = max2 = max3 = float('-inf')
+        min1 = min2 = float('inf')
+        for num in nums:
+            if num > max1:
+                max1, max2, max3 = num, max1, max2
+            elif num > max2:
+                max2, max3 = num, max2
+            elif num > max3:
+                max3 = num
             
-        #     return nums[0] * nums[1]* nums[-1]
-
-        # return nums[-1] * nums[-2] * nums[-3]
-        return max( nums[0] * nums[1]* nums[-1], nums[-1] * nums[-2] * nums[-3])
-
+            if num < min1:
+                min1, min2 = num, min1
+            elif num < min2:
+                min2 = num
+        return max(min1 * min2 * max1, max1* max2* max3)
+                 
 
 # Tes case all pso9v top 3
 # all negavtive top3 
 # 2  negativ 1  positve
+
+# 2 case
+# all max 3 
+# 2 sammalled 1 lar
