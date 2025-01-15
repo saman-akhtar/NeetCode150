e7a4eae0673d2b74042b9ce693adc7eae1c76ac1
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-01-15 08:21:03 -0800

    Time: 1 ms (61.07%) | Memory: 18 MB (13.32%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,8 +1,8 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        letter = {}
-        for i in s:
-            letter[i] = letter.get(i,0) + 1
+        letter = Counter(s)
+        # for i in s:
+            # letter[i] = letter.get(i,0) + 1
         count = 0
         odd_found = False
         for v in letter.values():
@@ -14,11 +14,11 @@ class Solution:
                     if v > 1:
                         count += v-1
                     odd_found = True 
-                # del letter[k]
         if odd_found:
             count += 1
         return count
 
-
+# TC O(N)
+# TC O(n)
 
         
