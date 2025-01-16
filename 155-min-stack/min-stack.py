983c6e5174df316c1d129f430eea510f37ffc351
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-01-16 05:21:57 -0800

    Time: 8 ms (39.58%) | Memory: 21.4 MB (24.57%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -1,80 +1,37 @@
 class MinStack:
-
+ # claryfing : if uniqye item added ?
     def __init__(self):
         self.stack = []
-        self.min = []
+        self.minStack = []
         
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        value = min (val, self.min[-1] if self.min else val)
-        self.min.append(value)
+        if self.minStack:
+            min_val = self.minStack[-1]
+        else:
+            min_val = float('inf')
+        self.minStack.append(min(val, min_val))
         
 
     def pop(self) -> None:
         if self.stack:
-            self.stack.pop()
-            self.min.pop()
-        else:
-            print("wrong operation")
-
+            self.minStack.pop()
+            last = self.stack.pop()
+            return last
+        print("Wrong operation")
         
 
     def top(self) -> int:
         if self.stack:
             return self.stack[-1]
-        
-        print("wrong operation")
-        return []
-        
+        print("Wrong operation")
 
     def getMin(self) -> int:
-        return self.min[-1]
-#TC O(1)
-# sc         
-        
-        
-# class MinStack:
-
-#     def __init__(self):
-#         self.stack = []
-#         self.min = float("infinity")
+        if self.stack:
+            return self.minStack[-1]
         
 
-#     def push(self, val: int) -> None:
-#         self.stack.append(val)
-#         self.min = min(self.min, val)
-        
-
-#     def pop(self) -> None:
-#         if len(self.stack) > 0:
-#             # add logic for calculating min
-#             ele = self.stack.pop()
-#             if(ele == self.min ):
-#                 if( len(self.stack) >0 ):
-#                     self.min = min(self.stack)
-#                 else:
-#                     self.min = float("infinity")
-#         else:
-#             print("wrong operation")
-
-        
-
-#     def top(self) -> int:
-#         le = len(self.stack)
-#         if le > 0:
-#             return self.stack[le-1]
-        
-#         print("wrong operation")
-#         return []
-        
-
-#     def getMin(self) -> int:
-#         return self.min
-        
-# in this approach, min is varible need to be updated whenever min item 
-# is popped from stack
-# so btter to have min as a stack
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
