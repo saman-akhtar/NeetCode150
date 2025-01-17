e27416b6b5f032d3b739e0dad309b10314a90622
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-01-17 06:27:44 -0800

    Time: 15 ms (81.96%) | Memory: 17.9 MB (28.31%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,18 +1,37 @@
 class Solution:
+    # APROACH 1
+    # def lengthOfLongestSubstring(self, s: str) -> int:
+    #     windLen = 0
+    #     l = 0
+    #     r = 0
+    #     wind = set()
+    #     while r < len(s):
+    #         while s[r]  in wind:
+    #                 wind.remove(s[l])
+    #                 l +=1
+    #         wind.add(s[r])
+    #         r += 1
+    #         windLen = max(windLen, r -l )
+    #     return windLen
+# TC outher O(N) for r & inner while lloop ch character is added and removed from the wind set only once  O(N) => O(N) + O(N) = 0(N)
+
+#SC O(min(n,a)) a is no of ch .. where n is the length of the string and a is the size of the character set (e.g., 26 for low
+    
+    # APproach 2
     def lengthOfLongestSubstring(self, s: str) -> int:
         windLen = 0
         l = 0
-        r = 0
-        wind = set()
-        # wind.add(s[0])
-        while r < len(s):
-            while s[r]  in wind:
-                    wind.remove(s[l])
-                    l +=1
-            # if s[r] not in wind:
-            wind.add(s[r])
-            r += 1
-            windLen = max(windLen, r -l )
-        return windLen
+        charIndex = {}
 
-        
+        for r in range(len(s)):
+            if s[r] in charIndex and charIndex[s[r]] >= l:
+                # Update left pointer to skip duplicate
+                l = charIndex[s[r]] + 1
+            
+            # Update the character's most recent index
+            charIndex[s[r]] = r
+
+            # Calculate the current window length
+            windLen = max(windLen, r - l + 1)
+
+        return windLen
