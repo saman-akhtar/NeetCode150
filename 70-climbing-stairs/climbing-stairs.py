f6622c8ca3c57a14e6eb132806c239b846367d97
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-01-18 06:22:45 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (26.53%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,11 +1,28 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp = [0] * (n+1)
-        dp[1] = 1
+        # dp = [0] * (n+1)
+        # dp[1] = 1
+
+        # # bottom up approach
+        # if n == 1:
+        #     return 1
+        # dp[2] = 2
+        # for i in range(3, len(dp)):
+        #     dp[i] = dp[i-2] + dp[i-1]
+        # return dp[n]
+       
+       # TC O(N)
+       # SC O(N)
+
+    # optimized
+        prev1 = 1
+        prev2 = 2
         if n == 1:
             return 1
-        dp[2] = 2
-        for i in range(3, len(dp)):
-            dp[i] = dp[i-2] + dp[i-1]
-        return dp[n]
-       
+        if n == 2:
+            return 2
+        for i in range(3, n+1):
+            cur = prev1 + prev2
+            prev1 = prev2
+            prev2 = cur
+        return prev2
