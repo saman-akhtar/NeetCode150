6965f97695ba68fa7b043aa1b49e91368cfe1278
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-01-18 10:10:15 -0800

    Time: 0 ms (100.00%) | Memory: 17.6 MB (40.53%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,41 +1,26 @@
-class Solution:
-    def decodeString(self, s: str) -> str:
-        def parseData(j):
-            stack = []
-            rep = 0
-            end_index = j
-            str_res = ""
-            
-            while j < len(s) and s[j] != "]":
-                if s[j].isdigit():
-                    rep = rep * 10 + int(s[j])
-                elif s[j] == "[":
-                    tmp_res, end_index = parseData(j + 1)
-                    # Apply repetition immediately when getting result from nested call
-                    str_res += tmp_res * rep
-                    rep = 0
-                    j = end_index  # Skip the processed substring
-                    continue
-                else:
-                    str_res += s[j]
-                j += 1
-                
-            return str_res, j + 1  # Return the processed string and the position after ']'
+class Solution(object):
+    def decodeString(self, s):
+        stack = []  # To store previous strings and their multipliers
+        num = 0  # To store the current number
+        temp = ""  # To store the current decoded string
+
+        for i in s:
+            if i.isdigit():  # to handle multi-digit numbers
+                num = (num * 10) + int(i)
+
+            elif i == '[':  # Push the current string and multiplier onto the stack
+                stack.append((temp, num))
+                # Reset num and temp for the next segment
+                num = 0  
+                temp = ""  
+
+            elif i == ']':  # Pop from stack to decode the substring
+                string, nums = stack.pop()
+                temp = string + (temp * nums)  # Repeat the substring and concatenate
+
+            else:  # Append alphabets to the current string
+                temp += i
+
+        return temp
+
         
-        # Main function logic
-        rep = 0
-        res = ""
-        i = 0
-        while i < len(s):
-            if s[i].isdigit():
-                rep = rep * 10 + int(s[i])
-            elif s[i] == "[":
-                new_str, j = parseData(i + 1)
-                res += new_str * rep
-                rep = 0
-                i = j - 1
-            else:
-                res += s[i]
-            i += 1
-            
-        return res
