be964804691fc59bda0b3027d94635b9e022b063
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-01-19 12:00:36 -0800

    Time: 7 ms (46.65%) | Memory: 21.3 MB (5.58%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -6,54 +6,19 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        
-        # Approach 2 
-        # optimized
-        def dfs(root):
+
+        def dfs( root):
             if not root:
-                return 0, 0
-
-            maxlh, maxLdia = dfs(root.left)
-            maxrh, maxRdia = dfs(root.right)
-
-                # cur height
-            height = 1 + max(maxlh, maxrh)
-            # cur dia
-            cur_dia = maxlh + maxrh
-            dia = max(cur_dia, maxLdia,maxRdia )
-            return height, dia
-        height, dia = dfs(root)
+                return 0,0
+            
+            maxLdia,left = dfs(root.left)
+            maxRdia, right = dfs(root.right)
+            height = max(left, right) + 1
+            cur_dia = left + right
+            dia = max(cur_dia, maxRdia, maxLdia)
+            return dia, height
+        dia, height = dfs(root)
         return dia
 
 
-
-
-
-
-        
-        
-        # Approach1
-
-        # callculat diamter for every node 
-        # dia = max Left height + max right height
-        # to calculate that use 1 more recursion func: which calculate max of left height , right height
-
-
-        # if not root:
-    #             return 0
-    #     maxLeft = self.findMaxdia(root.left)
-    #     maxright = self.findMaxdia(root.right)
-    #     dia =  maxLeft + maxright
-    #     cur_dia = max(self.diameterOfBinaryTree(root.left), self.diameterOfBinaryTree(root.right))
-    #     return max(dia,cur_dia )
-        
-    # def findMaxdia(self, root):
-    #     if not root:
-    #         return 0
-    #     cur_dia = 1 + max(self.findMaxdia(root.left) , self.findMaxdia(root.right))
-    #     return cur_dia        
-            
-    # tc O n ^2
-    # sc O (N)
-        
-        
+       
