3436ddf08aeb589d4320df0e67d227f362a4a48d
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-01-19 09:06:34 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (23.66%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -23,3 +23,16 @@ class Solution:
         if digits:  # Only start backtracking if digits is not empty
             backtrack(0, "")
         return res
+
+# TC Therefore, there are 
+
+#   combinations, and each combination requires O(n) to store (as strings are immutable in Python and must be copied).
+# Overall Time Complexity:\U0001d442(\U0001d458 ^ \U0001d45b⋅\U0001d45b)
+
+# Where: 
+# k=3 or 4
+# 4 (letters mapped to a digit, e.g., 7 maps to 4 letters, others map to 3).
+# \U0001d45b
+# n is the number of digits in the input.
+
+# SCO(k ^n⋅n) == each stack if od dept n & store k ^ n combo
