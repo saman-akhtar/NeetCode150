85c13caf707962c16490b78f24e0b675438188ae
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-01-19 10:40:53 -0800

    Time: 1 ms (45.63%) | Memory: 17.9 MB (53.54%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,25 +1,22 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        # to form each valid bracktes
-        stack = []
-
-        # store all the brackets
         res = []
-        def backtrack(openP, closeP):
-            if openP == closeP == n:
-                s = "".join(stack)
-                res.append(s)
+
+        def backtrack(cur_str, oc, cc):
+            if oc == 0 and cc == 0:
+                res.append(cur_str)
                 return
-            if openP < n:
-                stack.append("(")
-                backtrack(openP + 1, closeP)
-                stack.pop()
-            
-            if closeP < openP:
-                stack.append(")")
-                backtrack(openP , closeP +1)
-                stack.pop()
-        backtrack(0,0)
+            if oc > 0:
+                # cur_str += "("
+                # oc -=1
+                # cc +=1
+                backtrack(cur_str + "(", oc-1,cc+1)
+
+            if cc > 0:
+                # cc -=1
+                backtrack(cur_str + ")", oc,cc-1)
+
+        backtrack("",n,0)
         return res
 
         
