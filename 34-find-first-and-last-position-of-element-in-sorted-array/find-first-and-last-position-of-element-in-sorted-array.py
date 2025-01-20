b5a3d01fa1919628b29acd595d710a296fef697a
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-01-20 08:07:03 -0800

    Time: 0 ms (100.00%) | Memory: 19 MB (31.74%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,24 +1,54 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        l = 0 
-        h = len(nums) -1
-        while l <= h:
-            mid = (l+ h)//2
-            print(mid)
-            if (nums[mid]== target):
-                i = mid
-                j = mid
-                while( i >=0 and nums[i]== target):
-                    i -= 1
-                while( j <len(nums) and nums[j]== target):
-                    j += 1
-                i = i + 1
-                j = j -1
-                return [i,j]
-            elif (nums[mid] > target):
-                h = mid -1
-            else:
-                l = mid + 1
-        return [-1,-1]
 
+        # approach 2
+        
+            def findBound(isFirst):
+                bound = -1
+                l = 0 
+                h = len(nums) -1
+
+                while l <= h:
+                    mid = (l+ h)//2
+                    if (nums[mid]== target):
+                        bound = mid
+                        if isFirst:
+                            h = mid -1
+                        else:
+                            l = mid + 1
+                    elif (nums[mid] > target):
+                        h = mid -1
+                    else:
+                        l = mid + 1
+                return bound
+            first = findBound(True)
+            if first == -1:
+                return [-1, -1]
+            last = findBound(False)
+            return [first, last]
+
+
+# APPROACH 1
+
+
+#         l = 0 
+#         h = len(nums) -1
+#         while l <= h:
+#             mid = (l+ h)//2
+#             if (nums[mid]== target):
+#                 i = mid
+#                 j = mid
+#                 while( i >=0 and nums[i]== target):
+#                     i -= 1
+#                 while( j <len(nums) and nums[j]== target):
+#                     j += 1
+#                 i = i + 1
+#                 j = j -1
+#                 return [i,j]
+#             elif (nums[mid] > target):
+#                 h = mid -1
+#             else:
+#                 l = mid + 1
+#         return [-1,-1]
+#  # TC O(logn + N)
 
