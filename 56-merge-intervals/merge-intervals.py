49c844103a3d5911bb5b7c7c89f9575a7885884f
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-01-22 06:55:32 -0800

    Time: 9 ms (42.35%) | Memory: 20.8 MB (72.03%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -2,25 +2,42 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
 
         #CLARYIFNg : is it sorted
+        # APproach 2
         intervals.sort(key= lambda x : x[0])
         res = []
-        # for i in range(len(intervals)-1):
         i = 0
-        while i < len(intervals): 
-            if i == len(intervals)-1:
-                # res.append(intervals[i])
-                break
-            end_cur = intervals[i][1]
-            start_next = intervals[i+ 1][0]
-            if ( end_cur >= start_next):
-                intervals[i][1] = max(end_cur,intervals[i+ 1][1])
-                del intervals[i+1]
-                # res.append([intervals[i][0],max(end_cur,intervals[i+ 1][1])])
-                # i += 1
+        while i < len(intervals):
+            # no overlapp  or no res
+            if not res or res[-1][1] < intervals[i][0]:
+                res.append(intervals[i])
             else:
-                # res.append(intervals[i])
-                i += 1
-        return intervals
+                res[-1][1] = max(intervals[i][1],res[-1][1] )
+            i += 1
+        return res
 
+        # TC  O(nlogn) + O(N))
+        # SC O(N)
+
+
+
+
+        # APproach 1
+        # intervals.sort(key= lambda x : x[0])
+        # res = []
+        # i = 0
+        # while i < len(intervals): 
+        #     if i == len(intervals)-1:
+        #         break
+        #     end_cur = intervals[i][1]
+        #     start_next = intervals[i+ 1][0]
+        #     if ( end_cur >= start_next):
+        #         intervals[i][1] = max(end_cur,intervals[i+ 1][1])
+        #         del intervals[i+1]
+        #     else:
+        #         i += 1
+        # return intervals
+# TC  O(nlogn) +  O(N *N)
+
+# SC O(1)
 
         
