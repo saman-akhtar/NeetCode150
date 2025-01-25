edc2cd2d0af968a923076f2242991a7c1553ea45
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-01-25 11:44:17 -0800

    Time: 93 ms (46.19%) | Memory: 28.8 MB (35.66%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,13 +1,34 @@
 class Solution:
 
     # clarifying question : is k valid ? k wont exceed array size
+
+    #
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        heap =[]
+
+        # APPROACH 1
+        # nums = [-1 * i for i in nums]
+        # heapq.heapify(nums)
+        # largtest = 0
+
+        # while k > 0:
+        #     largtest = -1 * heapq.heappop(nums)
+        #     k -=1
+        # return largtest
+
+# O(n+k⋅logn)
+# SC O(N)
+        # APPROACH 2
+        heap = []
         for i in nums:
-            heapq.heappush(heap,i)
-            if (len(heap) > k):
+            heapq.heappush(heap, i)
+            if(len(heap) > k):
                 heapq.heappop(heap)
         return heapq.heappop(heap)
+
+       
+
+# TC O Nlog k
+# SC O K
             
 
         
