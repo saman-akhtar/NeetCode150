61bc80e10a10927e2c0044a47ac1c8dfe3abcde2
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-01-26 07:56:30 -0800

    Time: 6 ms (55.70%) | Memory: 19.4 MB (30.79%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,38 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        visit = set()
+        preMap = {i:[] for i in range(numCourses)}
+        # can same pre-re be there?
+        for course in prerequisites:
+            preMap[course[0]].append(course[1])
+
+            # 1 2 3.     1 2 3
+            #            2 1
+            # 2 3
+            # 3 []
+        def dfs(crs):
+            if crs in visit:
+                return False
+            if(preMap[crs] == []):
+                    return True
+            visit.add(crs)
+            for c in preMap[crs]:
+                
+                
+                if dfs(c)== False:
+                    return False
+            preMap[crs] = []
+            visit.remove(crs)
+            return True
+
+
+        for crs in preMap:
+            if dfs(crs) == False:
+                return False
+        return True
+
+
+
+
+
+        
