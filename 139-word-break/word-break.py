38039d3af82b15270ecd35992c5a9ec1715a88f5
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-01-28 09:32:49 -0800

    Time: 4 ms (41.28%) | Memory: 18.1 MB (11.07%) - LeetSync

---
+++ 139-word-break/word-break.py
@@ -0,0 +1,38 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # cats and og
+        # cat sandog. sand i
+
+        # APPROACH 2:
+        memo = {}
+        def canBreak(s):
+            # base case
+            if not s:
+                return True
+            if  s in memo:
+                return memo[s]
+            for word in wordDict:
+                if s.startswith(word):
+                    if canBreak(s[len(word):]):
+                        memo[s] = True
+                        return True
+            memo[s] = False
+            return False
+        return canBreak(s)
+
+
+        # APPROACH 1
+        # def canBreak(s):
+        #     # base case
+        #     if not s:
+        #         return True
+        #     for word in wordDict:
+        #         if s.startswith(word):
+        #             if canBreak(s[len(word):]):
+        #                 return True
+        #     return False
+        # return canBreak(s)
+
+        # TC k branch & depth n = > o(k ^N) k words in the dictionary, n characters in the string 
+        # SC depth n=> O(N)
+        
