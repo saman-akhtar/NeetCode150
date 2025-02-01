f8da7681ac8aafa6975ed64514013aaac01bdc26
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-02-01 13:24:01 -0800

    Time: 21 ms (37.77%) | Memory: 19.5 MB (11.24%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,29 +1,27 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        n = len(height)
-        maxl = height[0]
-        maxr = height[n-1]
-        l,r = 0, n-1
-        vol = 0
-        while l < r:
-            if maxl < maxr:
-                cur_water = maxl-height[l]
-                if (cur_water > 0):
-                    vol += cur_water
-                l +=1
-                maxl = max(height[l],maxl)
-                
-            else:
-                cur_water = maxr-height[r]
-                if (cur_water > 0):
-                    vol += cur_water
-                r -=1
-                maxr = max(height[r],maxr)
-                
-        return vol
+        water = 0
+# 1.            0  2  1  0  1  3. 2
+#         0  0  1  1  2  2  2  2  2
+#         3  3  3  3  3  3  3  3  0
+#         0. 0  1  0. 1  2  1  -1. 0
+        maxl = []
+        maxr = [0] * len(height)
+        curl = 0
+        for i in range( len(height)):
+            maxl.append(curl)
+            curl = max(height[i], curl)
+        curr = 0
+        for i in range( len(height)-1,-1,-1):
+            maxr[i] = curr
+            curr = max(height[i], curr)
+        for i in range( len(height)):
+            cur_w = min(maxl[i], maxr[i]) - height[i]
+            if cur_w > 0:
+                water += cur_w
 
-       
-    # TC O(N)
-    # SC (O(1))
-            
+        return water
+
+
+        
         
