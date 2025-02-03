f1f9d9899b6b7aed85599b12b9fa735fac7f70ec
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-02-03 06:18:17 -0800

    Time: 5 ms (1.89%) | Memory: 18 MB (8.05%) - LeetSync

---
+++ 62-unique-paths/unique-paths.py
@@ -0,0 +1,23 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        ROWS, COLS = m-1, n-1
+        grid = [[0] * n for i in range(m)]
+        grid[m-1][n-1] = 1
+        target = grid[m-1][n-1]
+        # visit
+        memo = {}
+        self.count = 0
+        def dp(i, j):
+            if i not in range(m) or j not in range(n):
+                return 0
+            if grid[i][j] == 1:
+                # self.count += 1
+                return 1
+            if (i,j) in memo:
+                return memo[(i,j)]
+            
+            memo[(i,j)] = dp( i, j +1) + dp(i + 1, j)
+            return memo[(i,j)]
+             
+        return dp(0, 0)
+        
