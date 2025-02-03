25e1ade4d36cd0a2c17cca49ae013a43a8f41b36
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-02-03 08:40:12 -0800

    Time: 0 ms (100.00%) | Memory: 17.6 MB (82.07%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,22 +1,38 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        ROWS, COLS = m-1, n-1
+        # DP 
         grid = [[0] * n for i in range(m)]
-        grid[m-1][n-1] = 1
-        memo = {}
-        self.count = 0
-        def dp(i, j):
-            if i not in range(m) or j not in range(n):
-                return 0
-            if grid[i][j] == 1:
-                return 1
-            if (i,j) in memo:
-                return memo[(i,j)]
+        for i in range(m):
+            grid[i][n-1] =1
+
+        
+        for j in range (n):
+            grid[m-1][j] =1
+        
+        for row in range(m-2,-1,-1):
+            for col in range(n-2,-1,-1):
+                grid[row][col]=grid[row+1][col]+grid[row][col+1]
+        return grid[0][0]
+
+
+        # MEMOIZATION
+        # ROWS, COLS = m-1, n-1
+        # grid = [[0] * n for i in range(m)]
+        # grid[m-1][n-1] = 1
+        # memo = {}
+        # self.count = 0
+        # def dp(i, j):
+        #     if i not in range(m) or j not in range(n):
+        #         return 0
+        #     if grid[i][j] == 1:
+        #         return 1
+        #     if (i,j) in memo:
+        #         return memo[(i,j)]
             
-            memo[(i,j)] = dp( i, j +1) + dp(i + 1, j)
-            return memo[(i,j)]
+        #     memo[(i,j)] = dp( i, j +1) + dp(i + 1, j)
+        #     return memo[(i,j)]
              
-        return dp(0, 0)
+        # return dp(0, 0)
         
         # TC O (M * n)
-        # SC O(M * N)
+        # SC O(M * N) + O(N)
