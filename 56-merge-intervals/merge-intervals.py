86e715fe65b15c7ce993af1eaf3d23b4165c9359
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-02-04 08:00:50 -0800

    Time: 3 ms (93.20%) | Memory: 20.9 MB (57.72%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,43 +1,16 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-
-        #CLARYIFNg : is it sorted
-        # APproach 2
-        intervals.sort(key= lambda x : x[0])
-        res = []
-        i = 0
-        while i < len(intervals):
-            # no overlapp  or no res
-            if not res or res[-1][1] < intervals[i][0]:
-                res.append(intervals[i])
+        intervals.sort(key= lambda x:x[0])
+        res = [intervals[0]]
+        for i in range(1,len(intervals)):
+            cur_int = intervals[i]
+            if(cur_int[0] <= res[-1][1]):
+                res[-1][1] = max(cur_int[1], res[-1][1] )
             else:
-                res[-1][1] = max(intervals[i][1],res[-1][1] )
-            i += 1
+                res.append(cur_int)
+
         return res
 
-        # TC  O(nlogn) + O(N))
-        # SC O(N)
-
-
-
-
-        # APproach 1
-        # intervals.sort(key= lambda x : x[0])
-        # res = []
-        # i = 0
-        # while i < len(intervals): 
-        #     if i == len(intervals)-1:
-        #         break
-        #     end_cur = intervals[i][1]
-        #     start_next = intervals[i+ 1][0]
-        #     if ( end_cur >= start_next):
-        #         intervals[i][1] = max(end_cur,intervals[i+ 1][1])
-        #         del intervals[i+1]
-        #     else:
-        #         i += 1
-        # return intervals
-# TC  O(nlogn) +  O(N *N)
-
-# SC O(1)
-
+        # TC 
+        # SC
         
