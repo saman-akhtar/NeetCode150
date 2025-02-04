c0a2c06415abc937fe653c6cfe62a60810e022ed
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-02-04 07:25:14 -0800

    Time: 6 ms (12.83%) | Memory: 18 MB (23.97%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.py
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.py
@@ -1,17 +1,34 @@
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        # Better slicing techniqu
         n = len(groupSizes)
         groupMap = defaultdict(list)
-        for i in range(n):
-            group = groupSizes[i]
-            groupMap[group].append(i)
+        # O(N)
         res = []
-        for key, value in groupMap.items():
-            if len(value) > int(key):
-                for i in range(0, len(value), key ):
-                    res.append(value[i:i + key])
-
-            else:
-                res.append(value)
-        return res
+        for i, size in enumerate(groupSizes):
+            groupMap[size].append(i)
+            if (len( groupMap[size]) == size):
+                res.append(groupMap[size])
+                groupMap[size] = []
         
+        return res
+
+
+        # n = len(groupSizes)
+        # groupMap = defaultdict(list)
+        # # O(N)
+        # for i in range(n):
+        #     group = groupSizes[i]
+        #     groupMap[group].append(i)
+        # res = []
+        # # O(N)
+        # for key, value in groupMap.items():
+        #     if len(value) > int(key):
+        #         for i in range(0, len(value), key ):
+        #             res.append(value[i:i + key])
+
+        #     else:
+        #         res.append(value)
+        # return res
+        # TC O(N) + O(N).O(M)
+        # SC O(N)
