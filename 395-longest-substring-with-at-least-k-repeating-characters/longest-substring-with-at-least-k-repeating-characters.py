cc6346f1a9125f8a79c3826c144d98d50cbf0f91
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-02-05 07:18:41 -0800

    Time: 802 ms (16.48%) | Memory: 28.6 MB (21.78%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
@@ -6,21 +6,37 @@ class Solution:
         # logic if any freq of ch < k , that wont be included in part of substring
         # so start new substring
         
-        l = 0 
-        chMap = Counter(s)
-        maxLen = 0
-        valid = True
-        for r in range(len(s)):
-            if chMap[s[r]] > 0 and chMap[s[r]] < k:
-                substring = s[l:r]
-                maxLen = max(maxLen, self.longestSubstring(substring, k))
-                valid = False
-                l = r + 1
-        if (valid):
-            return len(s)
-        else:
-            return max(maxLen,self.longestSubstring(s[l:], k))
+        # l = 0 
+        # # The Counter(s) operation takes O(N).
 
+        # chMap = Counter(s)
+        # maxLen = 0
+        # valid = True
+        # # O(N)
+        # for r in range(len(s)):
+        #     # In the worst case, each recursive call operates on roughly half the original input (T(N) ≈ 2T(N/2) + O(N)), leading to O(N log N).
+        #     if chMap[s[r]] > 0 and chMap[s[r]] < k:
+        #         substring = s[l:r]
+        #         maxLen = max(maxLen, self.longestSubstring(substring, k))
+        #         valid = False
+        #         l = r + 1
+        # if (valid):
+        #     return len(s)
+        # else:
+        #     return max(maxLen,self.longestSubstring(s[l:], k))
+
+        def helper(left, right):
+            if right - left < k:
+                return 0
+            
+            chMap = Counter(s[left:right])
+            for mid in range(left, right):
+                if chMap[s[mid]] > 0 and chMap[s[mid]] < k:
+                    return max(helper(left, mid), helper(mid + 1, right))
+                    
+            return right - left
+        
+        return helper(0, len(s))
 
         # while l <= r:
         #     # r += 1
