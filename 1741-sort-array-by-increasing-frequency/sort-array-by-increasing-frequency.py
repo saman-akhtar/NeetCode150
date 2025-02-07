ef75c34c54556b469e63c941574edcafeb816edc
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-02-07 08:14:11 -0800

    Time: 4 ms (29.86%) | Memory: 17.8 MB (41.14%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.py
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.py
@@ -2,10 +2,20 @@ class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
         numMap = Counter(nums)
 
+        #
         sorted_list =sorted(nums, key=lambda x: (numMap[x], -x) )
         return sorted_list
 
         # TC O(NLOGN)
+        # SC O(N)
+
+        numMap = Counter(nums)
+
+        sorted_list =sorted(numMap.items(), key=lambda x: (x[1], -x[0]) )
+        res = [num for num, count in sorted_list for _ in range(count)]
+        return res
+
+        # TC O(N) + O(NLOGN) + o(N)= O(N log N).
         # SC O(N)
 
         
