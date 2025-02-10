c1ecbc25018953b0ccf4f3c0eb599838e699d631
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-02-09 17:51:24 -0800

    Time: 1345 ms (10.91%) | Memory: 58.6 MB (34.91%) - LeetSync

--- 1332-count-vowels-permutation/count-vowels-permutation.py
+++ 1332-count-vowels-permutation/count-vowels-permutation.py
@@ -2,27 +2,72 @@ class Solution:
     def countVowelPermutation(self, n: int) -> int:
         MOD = 10**9 + 7
         vowels = {'a': ['e'],'e':['a','i'],'i':['a','e','o','u'],'o':['i','u'],'u':['a']}
-        res = set()
-        self.count = 0
         memo = {}
-        def createVowel( cur_len,prev):
-            if(cur_len ==n):
-              
+        
+        def dp(cur_str, length):
+            if (length == n):
                 return 1
-            if (cur_len, prev) in memo:
-                return memo[(cur_len, prev)]
+
+            if (cur_str, length) in memo:
+                return memo[(cur_str, length)]
             count = 0
-            if prev == "":
-                for key in vowels:
-                    count = (count + createVowel(cur_len + 1, key)) % MOD
+            
+            if cur_str == "":
+                for key,val in vowels.items():
+                    count =( count + dp(key ,1)) % MOD
             else:
-                for nextV in vowels[prev]:
-                        count = (count + createVowel(cur_len + 1, nextV)) % MOD
-            memo[(cur_len, prev)] = count
-            return count
+                for s in vowels[cur_str]:
+                        count =( count + dp(s, length + 1 )) % MOD
+            memo[(cur_str, length)] =count
+            return memo[(cur_str, length)]
+
+        return dp("", 0)
+        # MOD = 10**9 + 7
+        # vowels = {'a': ['e'],'e':['a','i'],'i':['a','e','o','u'],'o':['i','u'],'u':['a']}
+        
+        # def dp(cur_str, length):
+        #     if (length == n):
+        #         return 1
+        #     count = 0
+            
+        #     if cur_str == "":
+        #         for key,val in vowels.items():
+        #             count =( count + dp(key ,1)) % MOD
+        #     else:
+        #         for s in vowels[cur_str]:
+        #                 count =( count + dp(s, length + 1 )) % MOD
+
+        #     return count
+
+        # return dp("", 0)
+        # TC (O(5ⁿ)) + strig concation is N ^2 => 5 vowel 5 branch is each leve
+        # # Total Worst-Case Space Complexity (O(5ⁿ) ×N)+O(N)
 
 
-        return createVowel( 0, "")
+
+        # MOD = 10**9 + 7
+        # vowels = {'a': ['e'],'e':['a','i'],'i':['a','e','o','u'],'o':['i','u'],'u':['a']}
+        # res = set()
+        # self.count = 0
+        # memo = {}
+        # def createVowel( cur_len,prev):
+        #     if(cur_len ==n):
+              
+        #         return 1
+        #     if (cur_len, prev) in memo:
+        #         return memo[(cur_len, prev)]
+        #     count = 0
+        #     if prev == "":
+        #         for key in vowels:
+        #             count = (count + createVowel(cur_len + 1, key)) % MOD
+        #     else:
+        #         for nextV in vowels[prev]:
+        #                 count = (count + createVowel(cur_len + 1, nextV)) % MOD
+        #     memo[(cur_len, prev)] = count
+        #     return count
+
+
+        # return createVowel( 0, "")
 
         # TC O(N * 5) = O(N)
         # SC O(N * 5) = O(N)
