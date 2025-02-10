8220916ac2716e490601b934295054bfd2fe53c0
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-02-09 17:30:50 -0800

    Time: 0 ms (100.00%) | Memory: 18.7 MB (24.38%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -10,6 +10,11 @@ class Solution:
                 l = m +1
             else:
                 h = m - 1
-        print(l,h,m)
         return l
-        
+
+    # Throughout the loop, all elements at indices less than l are guaranteed to be less than the target, and all elements at indices greater than h are guaranteed to be greater than the target.
+    # so as  sson as l > h , we can add new elemnt at l as it will be = taget & it wont effect
+    # so in worst case l = len(nums) = end of array, so item instred there
+
+    # TC O(Log n)
+    # SC O(1)
