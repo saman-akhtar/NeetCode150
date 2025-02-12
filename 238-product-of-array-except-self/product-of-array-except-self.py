bcbb765f2cb65105296c6b4af824b3b682c30885
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-02-12 08:52:16 -0800

    Time: 23 ms (65.04%) | Memory: 23.3 MB (54.20%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,21 +1,29 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
 
-        
-        # 1,  2,  3, 4
-        # 1   1   2  6
-        # 24  12  4  1 
-        # 24. 12.  8 6
-        left_prod = [1] *len(nums)
-        right_prod= [1] *len(nums)
-        res = [1] *len(nums)
+        # CAN it fit into 32 bit ?
+        # can each element be negative positive
+        # check 32 bit overflow python, how to resolve it
+        # 1.  2. 3. 4. intial
+        # 1   1  2    6 pre
+        # 24  12  4   1  post
+        # 24 12.  8   6
+
+        # -1  1   0  -3    3
+        # 1. -1  -1   0    0
+        # 0   0   -9  3    1
+        # 0   0.   9. 0.   0
+
+        # 1.  -1. -1  0  
+        prefix = 1
+        n = len(nums)
+        res = [1] * n
         for i in range(1, len(nums)):
-
             res[i] = res[i-1] * nums[i-1]
-
-        for i in (range(len(nums)-2, -1, -1)):
-            right_prod[i] = right_prod[i+1] * nums[i+1]
-
-        for i in range(len(nums)):
-            res[i]= res[i]* right_prod[i] 
+        postfix = 1
+        for i in range(n -1 , -1,-1):
+            res[i]= postfix * res[i]
+            postfix = postfix * nums[i]
         return res
+
+        
