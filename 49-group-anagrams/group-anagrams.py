2e9c10a3370e4dbfc4e8d7ba592a0e48230bc4e9
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-02-20 08:56:53 -0800

    Time: 18 ms (40.91%) | Memory: 22.7 MB (25.87%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,17 +1,16 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         map = defaultdict(list)
+        # TC  O(N)
         for st in strs:
             nst = [0] * 26
+            # O(26)
             for s in st:
                 index = ord(s)- ord('a')
                 nst[index] = nst[index] + 1
             map[tuple(nst)].append(st)
         res = []
-        for key, val in map.items():
-            if len(val) > 0:
-                res.append(val)
-        return res
+        return list(map.values())
             
         
      
