7cd6685e8ce05b0814e7e170383bf1e546c4166c
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-03-08 15:10:22 -0800

    Time: 15 ms (82.36%) | Memory: 17.8 MB (82.26%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,4 +1,17 @@
 class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l = 0
+        windL = 0
+        wind = set()
+        for r in range(len(s)):
+            while s[r] in wind:
+                wind.remove(s[l])
+                l += 1
+            wind.add(s[r])
+            windL = max(windL, len(wind))
+        return windL
+
+
     # APROACH 1
     # def lengthOfLongestSubstring(self, s: str) -> int:
     #     windLen = 0
@@ -18,20 +31,20 @@ class Solution:
 #SC O(min(n,a)) a is no of ch .. where n is the length of the string and a is the size of the character set (e.g., 26 for low
     
     # APproach 2
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        windLen = 0
-        l = 0
-        charIndex = {}
+    # def lengthOfLongestSubstring(self, s: str) -> int:
+    #     windLen = 0
+    #     l = 0
+    #     charIndex = {}
 
-        for r in range(len(s)):
-            if s[r] in charIndex and charIndex[s[r]] >= l:
-                # Update left pointer to skip duplicate
-                l = charIndex[s[r]] + 1
+    #     for r in range(len(s)):
+    #         if s[r] in charIndex and charIndex[s[r]] >= l:
+    #             # Update left pointer to skip duplicate
+    #             l = charIndex[s[r]] + 1
             
-            # Update the character's most recent index
-            charIndex[s[r]] = r
+    #         # Update the character's most recent index
+    #         charIndex[s[r]] = r
 
-            # Calculate the current window length
-            windLen = max(windLen, r - l + 1)
+    #         # Calculate the current window length
+    #         windLen = max(windLen, r - l + 1)
 
-        return windLen
+    #     return windLen
