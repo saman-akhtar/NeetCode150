6e762625a536284fede1d881e0b71779f8464772
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-03-08 16:15:50 -0800

    Time: 169 ms (38.52%) | Memory: 17.9 MB (87.01%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,23 +1,36 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        #approch 1
-        count = {}
+
         l = 0
-        res = 0
-        maxf = 0
-        #O(N)
-        for r in range(len(s)):
-            count[s[r]] = 1 + count.get(s[r], 0)
-            #Optimization
-            maxf = max(maxf,  count[s[r]])
-            
-            # while not valid 
-       
-            while (r -l +1 - maxf) > k:
-                count[s[l]] -= 1
-                l += 1
-            res = max(r-l +1,res)
+        map = {}
+        res =0
+        for r, ch in enumerate(s):
+            map[ch]= map.get(ch, 0) + 1
+            while( r- l + 1 - max(map.values()) > k):
+                map[s[l]] -= 1
+                l +=1
+            res = max(res, r-l +1)
         return res
+            
+
+        # #approch 1
+        # count = {}
+        # l = 0
+        # res = 0
+        # maxf = 0
+        # #O(N)
+        # for r in range(len(s)):
+        #     count[s[r]] = 1 + count.get(s[r], 0)
+        #     #Optimization
+        #     maxf = max(maxf,  count[s[r]])
+            
+        #     # while not valid 
+       
+        #     while (r -l +1 - maxf) > k:
+        #         count[s[l]] -= 1
+        #         l += 1
+        #     res = max(r-l +1,res)
+        # return res
 
 #        Time Complexity: O(N)
 #        Space Complexity: O(1) (if character set is fixed, like English alphabet), otherwise O(C) where C is the number of distinct characters.
