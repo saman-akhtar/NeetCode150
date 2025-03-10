a70e9f989c02c853dda680edc8da29b0ac5996ea
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-03-10 08:26:37 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (80.87%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,15 +1,45 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        for bracket in s:
-            if ( bracket not in [")","]","}"]):
-                stack.append(bracket)
-            else:
-                if(stack):
-                    old_bracket = stack.pop()
-                    if( bracket == ")" and old_bracket == "(" or bracket == "}" and old_bracket == "{" or  bracket == "]" and old_bracket == "["):
-                        continue
-                    return False
+        for ch in s:
+            if ch in ["}",")","]"]:
+                if stack:
+                    old_ch = stack.pop()
+                    if ch == "}" and old_ch != "{":
+                        return False
+                    elif ch == ")" and old_ch != "(":
+                        return False
+                    elif ch == "]" and old_ch != "[":
+                        return False
+                    
+                    
                 else:
                     return False
-        return not stack
+            else:
+                stack.append(ch)
+        if stack:
+            return False
+        return True
+
+
+
+
+
+
+
+
+        # stack = []
+        # for bracket in s:
+        #     if ( bracket not in [")","]","}"]):
+        #         stack.append(bracket)
+        #     else:
+        #         if(stack):
+        #             old_bracket = stack.pop()
+        #             if( bracket == ")" and old_bracket == "(" or bracket == "}" and old_bracket == "{" or  bracket == "]" and old_bracket == "["):
+        #                 continue
+        #             return False
+        #         else:
+        #             return False
+        # return not stack
+# TC O(N)
+# SC O(N)
