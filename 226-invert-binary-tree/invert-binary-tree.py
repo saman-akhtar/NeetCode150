ab42b84619de544018d12f1c7d7eec1a8d67a503
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-03-15 12:52:43 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (32.08%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.py
+++ 226-invert-binary-tree/invert-binary-tree.py
@@ -8,16 +8,33 @@ class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
             return None
-        left = root.left
-        right = root.right
-        
-        root.left , root.right = root.right ,root.left
-
-        self.invertTree(root.left)
-
-        self.invertTree(root.right)
+        right = self.invertTree(root.right)
+        left = self.invertTree(root.left)
+        root.left ,root.right = right,left
         return root
+
+
+
+
+
+
+
+
+
+
+
+        # if not root:
+        #     return None
+        # left = root.left
+        # right = root.right
+        
+        # root.left , root.right = root.right ,root.left
+
+        # self.invertTree(root.left)
+
+        # self.invertTree(root.right)
+        # return root
 # TC O(N)    
-# SC O(N) 
+# SC O(N) (recursion stack)
         
         
