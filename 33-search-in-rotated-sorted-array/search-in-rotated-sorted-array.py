99db958232130f5dc2b194f99a4614ea111edfab
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-03-15 07:24:10 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (67.92%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,23 +1,30 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l,h = 0, len(nums)-1
-        while l <=h:
-            mid = (l + h)//2
-            if ( nums[mid] == target):
-                return mid
-            # determin which is sorted
-            if (nums[l] <= nums[mid]):
-                if ( nums[l] <= target < nums[mid]):
-                    h = mid -1
+        l = 0
+        h = len(nums) -1
+        while l <= h:
+            m = (l +h)//2
+            if nums[m]== target:
+                    return  m
+            elif nums[m] >= nums[l]:
+                if target < nums[l] or target > nums[m]:
+                    l = m + 1 # if not found in sorted
                 else:
-                    l = mid + 1
+                    h = m -1
+                # sorted part
             else:
-                # Target lies in the sorted right part
-                if ( nums[mid] < target <= nums[h]):
-                    l = mid +1
+                if target > nums[h] or target < nums[m]:
+                    h = m - 1 # if not found in sorted
                 else:
-                    h = mid -1
+                    l = m + 1
+
+            
+                
         return -1
+                
+
+
+# 4 5 6 0 1 2 3
 # O (Logn )
 # O(1)
         
