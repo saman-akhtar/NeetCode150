ca3a7e6402be4fb28c8efe533a336a1ef02e8d61
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-03-15 12:54:58 -0700

    Time: 0 ms (100.00%) | Memory: 18.1 MB (31.81%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,41 +1,23 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        l = 0
-        h = len(nums) -1
-        while l < h:
-            m = (l +h)//2
-            if nums[m] > nums[h]:
+
+
+        l, r = 0, len(nums) - 1
+        
+        while l < r:
+            m = (l + r) // 2
+            
+            # If the middle element is greater than the rightmost element,
+            # the minimum is in the right half
+        # go to unsorted part
+            if nums[m] > nums[r]:
                 l = m + 1
             else:
-                h = m
+                # We use r = m to include the middle element in our search range when there’s a possibility that it could be the minimum.
+                r = m
+        
+        # The loop ends when l == r, which is the index of the smallest element
         return nums[l]
-
-
-
-
-
-
-
-
-
-
-
-
-        # l, r = 0, len(nums) - 1
-        
-        # while l < r:
-        #     m = (l + r) // 2
-            
-        #     # If the middle element is greater than the rightmost element,
-        #     # the minimum is in the right half
-        #     if nums[m] > nums[r]:
-        #         l = m + 1
-        #     else:
-        #         # We use r = m to include the middle element in our search range when there’s a possibility that it could be the minimum.
-        #         r = m
-        
-        # # The loop ends when l == r, which is the index of the smallest element
-        # return nums[l]
 # Time Complexity: \U0001d442(log\U0001d45b)
 # O(logn), since we are using binary search.
 # Space Complexity: 
