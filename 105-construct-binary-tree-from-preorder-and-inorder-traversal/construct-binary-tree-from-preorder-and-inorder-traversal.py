cd20ccc48d0b13e224571a701a8d633736267c33
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-03-16 07:33:44 -0700

    Time: 96 ms (34.43%) | Memory: 89.2 MB (36.12%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -17,7 +17,25 @@
 # The function assumes preorder and inorder correspond to a valid binary tree. If this constraint isn't met, the tree construction won't be meaningful.
 
 class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:     
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:    
+        if not preorder  or not inorder:
+            return None
+        root = TreeNode(preorder[0])
+        index = inorder.index(preorder[0])
+        root.left = self.buildTree( preorder[1:index+1], inorder[:index]) 
+        right = self.buildTree( preorder[index +1:], inorder[index +1:]) 
+        root.right  = right
+        return root
+
+
+
+
+
+
+
+
+
+
         if not preorder or not inorder:
             return None
             
