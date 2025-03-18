f320697786fe0c4115f3545b2fc6b72e5ea30149
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-03-18 08:05:28 -0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (95.73%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,25 +1,35 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
 
-        # DP top down
+        # DP - top down
+        if n == 1:
+            return 1
+        prev1, prev2 = 1,2
+        for i in range(3, n + 1):
+            cur = prev2 + prev1
+            prev2,prev1 = cur, prev2
+        return prev2
 
-        # DP MEMOZATIOn
-        climb = {}
-        def memo(n):
+
+        # DP - MEMOZATIOn
+        # climb = {}
+        # def memo(n):
         
-            if n == 1:
-                return 1
-            elif n == 2: 
-                return 2
-            if n in climb:
-                return climb[n]
+        #     if n == 1:
+        #         return 1
+        #     elif n == 2: 
+        #         return 2
+        #     if n in climb:
+        #         return climb[n]
         
-            stair1 =  climb[n-1]  if n -1  in climb else memo(n-1)
-            stair2 =  climb[n-2] if n -2  in climb else memo(n-2)
-            climb[n]=  stair1 + stair2
-            return climb[n]
+        #     stair1 =  climb[n-1]  if n -1  in climb else memo(n-1)
+        #     stair2 =  climb[n-2] if n -2  in climb else memo(n-2)
+        #     climb[n]=  stair1 + stair2
+        #     return climb[n]
         
-        return memo(n)
+        # return memo(n)
+        #TC O(N): Each unique n is computed once and stored in climb
+        # SC O(N)(due to memoization dictionary + recursion depth)
 
         # RECURSION Approach 1
         # if n == 1:
@@ -27,7 +37,8 @@ class Solution:
         # elif n == 2: 
         #     return 2
         # return self.climbStairs(n-1) + self.climbStairs(n-2)
-
+        #. TC O(2 ^n)
+        # SC O(N)
 
 
 
@@ -42,7 +53,7 @@ class Solution:
         #     dp[i] = dp[i-2] + dp[i-1]
         # return dp[n]
        
-       # TC O(N)
+       # TC O(2 ^ N)
        # SC O(N)
 
     # optimized
