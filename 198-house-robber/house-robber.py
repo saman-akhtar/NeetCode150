1d2319c78da5abab56054a63630299ed6e866952
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-03-18 09:43:31 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (82.64%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,5 +1,19 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        #Top Down
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        prev1, prev2 = 0, 0
+
+        for num in nums:
+            cur = max(num + prev2, prev1)
+            prev2,prev1 =  prev1,cur
+        return prev1
+
+
+
 
         # MEMOIZATION
         memo ={}
