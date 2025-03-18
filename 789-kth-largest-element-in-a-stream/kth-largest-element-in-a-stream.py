fa1ea9c2db72628e5537cea52f7eab8b8b2f2d00
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-03-17 18:44:54 -0700

    Time: 9 ms (90.07%) | Memory: 23.7 MB (60.11%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
@@ -3,15 +3,15 @@ class KthLargest:
     def __init__(self, k: int, nums: List[int]):
         self.k = k
         self.minHeap = nums
-        heapq.heapify(self.minHeap)
-        while len(self.minHeap) > k:
-            heapq.heappop(nums)
+        heapq.heapify(self.minHeap) # ON
+        while len(self.minHeap) > k:  #O (K) +> OK(Klogn)
+            heapq.heappop(nums) # O(logn)
         
         
 
     def add(self, val: int) -> int:
-        heapq.heappush(self.minHeap,  val)
-        while len(self.minHeap) > self.k:
+        heapq.heappush(self.minHeap,  val) #O(logn)
+        while len(self.minHeap) > self.k: #O(logK)
             heapq.heappop(self.minHeap)
 
         return self.minHeap[0]
@@ -25,7 +25,7 @@ class KthLargest:
 
 # TC
 # _init = O(n) + O(logN) * (n-k)= O(nlogn).
-# add O(logk) * m .. heap atake log k as heap size is atmost k, m add operation
+# add O(logk) * m .. heap take log k as heap size is atmost k, m add operation
 
 #space
 # O(k) .. at max stores k elemets
