b6d791e26073a578043164b1271d9b4c8aafbe94
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-03-21 08:42:28 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (52.13%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,6 +1,21 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
 
+        # Dp
+        n = len(s)
+        if n == 0 or s[0] == '0':
+            return 0
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n+1):
+            if s[i-1] != '0':
+                dp[i] += dp[i-1]
+            # two digit
+            if 10 <= int(s[i-2:i] )<= 26:
+                dp[i] += dp[i-2] 
+        return dp[n]
+
         # Memoization
 
         res = []
@@ -27,6 +42,8 @@ class Solution:
             return memo[i]
         n = len(s)
         return dfs(n) 
+        # TC O(n)
+        # O(n) + O(n) = o(n)
 
 
         # recursion
