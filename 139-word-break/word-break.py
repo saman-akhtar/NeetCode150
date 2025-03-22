8b1d2812278aaecd6f25d02eccfeb4bb11dd1a70
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-03-22 15:51:13 -0700

    Time: 11 ms (6.51%) | Memory: 18.3 MB (9.24%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -2,6 +2,40 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         # cats and og
         # cat sandog. sand i
+        # n = len(s)
+        # wordSet = set(wordDict)
+        # dp = [True] * (n+1)
+        # for i in range(1,n+1):
+        #     for j in range(0, i):
+                
+        #         dp[i]= dp[i][j] and s[j: i] in wordSet
+        # return dp[n]
+
+        word ={}
+        wordSet = set(wordDict)
+        def cansegmentString(i ):
+            if i== 0:
+                # "" is semntable ?
+                return True
+            # check semnet s[o:i] ? chag is
+            if (i) in word:
+                return word[i]
+            for j in range(i):
+                # chekc if prefix is breakble & sufficx is breakable
+                # why j notj - :beauc Can the part before j be broken into valid words?
+                if cansegmentString(j) and s[j:i] in wordSet:
+                    word[i] = True
+                    return word[i]
+            word[i] = False
+            return word[i]
+        n = len(s)
+        return cansegmentString(n)
+
+
+
+
+
+
 
         # APPROACH 2:
         memo = {}
