6f8c5d04711169183d54d7eb6f999d040d497a1c
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-03-22 16:28:41 -0700

    Time: 1471 ms (28.98%) | Memory: 33.5 MB (38.11%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -17,7 +17,7 @@ class Solution:
             dp[i][0]= True
         for i in range(1,n +1):
             for j in range( 1,target + 1):
-                if nums[i-1] <= target:
+                if nums[i-1] <= j:
                     dp[i][j] = dp[i-1][j- nums[i-1]] or dp[i-1][j] 
                 else:
                     dp[i][j] = dp[i-1][j]
