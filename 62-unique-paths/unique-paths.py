4fd2fc1ecb74f34a998082d4a3e9f3bffd8e1d52
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-03-23 13:27:46 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (65.22%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -2,17 +2,33 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
 
         # UNBOUND KNAPSAKC
-        # Bottom up
-
+        # APPROACH 2 of bottom up
+    #   1) fill from 0 to m,n
         dp = [[0] * (n + 1) for _ in range(m +1)]
-        # 0 amount , no of way 1
-        # 0 coins 
+         # Base case: the closer to desination is 1
+        # bottom + right
+        dp[1][1] = 1
+
+        for i in range(1,m + 1):
+            for j in range(1,n + 1):
+                if i == 1 and j == 1:
+                    continue  # skip already set
+                if i > 0:
+                    dp[i][j] += dp[i - 1][j]
+                if j > 0:
+                    dp[i][j] += dp[i][j - 1]
+
+        return dp[m][n]
+
+        # Bottom up
+        # APRROACH 2 fill from m,n to 0
+
+        dp = [[0] * (n ) for _ in range(m)]
          # Base case: the closer to desination is 1
         # bottom + right
         dp[m-1][n-1] =1
         # dp[m-1][n-2] =1
         # dp[m-2][n-1] =1
-        print("DP", dp)
         # for i in range()
         for i in range(m-1,-1,-1):
             for j in range(n-1,-1,-1):
@@ -37,25 +53,7 @@ class Solution:
 #  2 1 1 0.    m[2][1]  m[2][0] m[1][1].    m[i-1][j-2]. m[i-2][j-1]
 #  3 0 0 0
         # DP 
-        grid = [[0] * n for i in range(m)]
-        for i in range(m):
-            grid[i][n-1] =1
-
-        
-        for j in range (n):
-            grid[m-1][j] =1
-        
-        for row in range(m-2,-1,-1):
-            for col in range(n-2,-1,-1):
-                grid[row][col]=grid[row+1][col]+grid[row][col+1]
-        return grid[0][0]
-
-         # Bottom up:from the bottom-right to the top-left computing the solution step by step.
-
-
-        # TC O(N * M)
-        # SC O(N * M)
-
+    
 
         # MEMOIZATION
 
