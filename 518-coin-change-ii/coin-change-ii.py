72644a50559e28e89945a8f691a732f870c2e5a8
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-03-23 00:33:30 -0700

    Time: 338 ms (44.28%) | Memory: 60.1 MB (33.74%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,12 +1,37 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
+
+        # UNBOUND KNAPSAKC
+        # Bottom up
+        n = len(coins)
+
+        dp = [[0] * (amount + 1) for _ in range(n +1)]
+        # 0 amount , no of way 1
+        # 0 coins 
+        for i in range(n+1):
+            dp[i][0]= 1
+        for i in range(1,n+1):
+            for j in range(1,amount+1):
+                if coins[i-1] <= j:
+                    dp[i][j]=  dp[i][j-coins[i-1]]+ dp[i-1][j]
+                else:
+                    dp[i][j]=dp[i-1][j]
+        return dp[n][amount]
+        # TC O( N * Amount)
+        # SC O( N * Amount)
+
+
+
+
+
+
         memo = {}
 
         def dp(i, cur_amt):
             if cur_amt == 0:
                 return 1
             if ( i < 0 or cur_amt < 0):
-                return 0 # inf
+                return 0 
             
             if (i,cur_amt) in memo:
                 return memo[(i, cur_amt)]
@@ -19,4 +44,7 @@ class Solution:
 
         n = len(coins)
         return dp(n-1, amount)
+
+# TC O(N * amyt)
+# SC O(N * amt)  + O(N)
         
