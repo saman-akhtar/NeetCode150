b51d0d9bfd7c812ad9f7bb2dcf3be89344f30d78
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-03-23 15:56:20 -0700

    Time: 3 ms (74.78%) | Memory: 18.6 MB (54.95%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,5 +1,29 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+
+
+        # BOttom up
+        n = len(prices)
+        if n <= 1:
+            return 0
+        dp = [[0]*2 for i in range(n+1)]
+        for i in range(n - 1, -1, -1):
+            for buying in [True, False]:
+                if buying:
+                    buy = dp[i + 1][False] - prices[i] if i + 1 < n else -prices[i]
+                    cooldown = dp[i + 1][True] if i + 1 < n else 0
+                    dp[i][1] = max(buy, cooldown)
+                else:
+                    sell = dp[i + 2][True] + prices[i] if i + 2 < n else prices[i]
+                    cooldown = dp[i + 1][False] if i + 1 < n else 0
+                    dp[i][0] = max(sell, cooldown)
+
+        return dp[0][1] # sell day
+
+
+
+
+        # TOP DOWN
         n = len(prices)
         if n <= 1:
             return 0
@@ -23,6 +47,8 @@ class Solution:
 
             
         return max_profit(0, True)
+        # O(2.n) o (n)
+        # SC O(2.n) O(n)
         
         
 
