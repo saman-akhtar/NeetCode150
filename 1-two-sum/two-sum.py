34472e9ae9e852eaaa3f239c3db2123d5bc6128f
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-03-25 17:50:59 -0700

    Time: 1 ms (59.48%) | Memory: 18.9 MB (34.38%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,13 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #clarify can 1 no exist twice ?
-        numMap = {}
-        for i, num in enumerate(nums):
+        # unique 
+        result = {}
+        for i,num in enumerate(nums):
             diff = target - num
-            if diff in numMap:
-                return [numMap[diff], i]
+            if  diff in result:
+                index = result[diff]
+                return [index, i ]
             else:
-                numMap[num] = i
+                result[num] = i
+        
         # TC O(N)
         # SC  O(N)
         
