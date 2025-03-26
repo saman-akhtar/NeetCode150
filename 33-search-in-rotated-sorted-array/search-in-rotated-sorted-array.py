d7253e7c3c592f3e73c9f8385393dc7985c1d2e6
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-03-26 06:26:59 -0700

    Time: 2 ms (5.47%) | Memory: 18.3 MB (23.09%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,5 +1,47 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+
+        l, r = 0 , len(nums)-1
+        while l <= r:
+            mid = (l + r) // 2
+            print("mid tar", mid, target)
+            if nums[mid] == target:
+                return mid
+            # sorted
+            # logic is search in sorted half
+            if nums[l] <= nums[mid]:
+                if nums[l]<= target < nums[mid]:
+                    r   = mid -1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] < target <= nums[r]:
+                    
+                    l = mid + 1
+                else:
+                    r = mid -1
+            # elif nums[r] > nums[mid]:
+            #     if target > nums[mid]:
+            #         l = mid + 1
+                    
+            #     else:
+            #         r   = mid -1
+                    
+            # # if nums[r] > nums[mid]:
+            # #  / unsorted ??
+            # else:
+            #     if target > nums[mid]:
+
+            #             l = mid + 1
+            #     else:
+            #         r = mid -1
+        return -1
+        # 3 4 5 6 7 0 1 2
+
+        # TC O(logn)
+        # SC O(1)
+
+
         l = 0
         h = len(nums) -1
         while l <= h:
