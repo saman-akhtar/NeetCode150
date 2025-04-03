f1c5a187fa17336797e47160dc6bafa63cc79b42
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-04-03 09:08:35 -0700

    Time: 7 ms (30.96%) | Memory: 18.8 MB (61.76%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -7,45 +7,16 @@
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         res = True
-        def calculate(root):
+        def checkHeight(root):
             nonlocal res
             if not root:
                 return 0
-            leftc = calculate(root.left)
-            rightc = calculate(root.right)
-            # Check height difference
-            if abs(leftc - rightc) >= 2:
+            left = checkHeight(root.left) 
+            right = checkHeight(root.right)
+            if abs(left -right) > 1:
                 res = False
-            return 1 +  max(leftc, rightc)
-        
-        calculate(root)
+            return 1+  max(left,right)
+
+        checkHeight(root)
         return res
-#Time Complexity: 
-# \U0001d442
-# (
-# \U0001d45b
-# )
-# O(n), where 
-# \U0001d45b
-# n is the number of nodes
-#
-#Space Complexity: 
-# \U0001d442
-# (
-# ℎ
-# )
-# O(h), where 
-# ℎ
-# h is the height of the tree.
-# \U0001d442
-# (
-# log
-# ⁡
-# \U0001d45b
-# )
-# O(logn) for a balanced tree.
-# \U0001d442
-# (
-# \U0001d45b
-# )
-# O(n) for a degenerate tree.
+        
