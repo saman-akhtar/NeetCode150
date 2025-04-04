8bd1eb2b86d5d04d85d82deec1cede64bb7eeef0
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-04-04 07:41:25 -0700

    Time: 53 ms (84.79%) | Memory: 21 MB (81.62%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,6 +7,35 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return root
+        if p.val < root.val < q.val:
+            return root
+        elif p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q) 
+        if p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q) 
+        
+     
+    
+        # asuuming p & q alway exist
+        # TC O(log N) O(logn)(for balanced BST)orO(n)(for skewed BST)
+        #.           he worst-case space complexity is:
+# O(logn)(for balanced BST)orO(n)(for skewed BST)
+
+        # @ SC O(logn)(for balanced BST)orO(n)(for skewed BST)
+#         The recursive stack uses O(h) space, where h is the height of the tree.
+
+# For a balanced BST, h = O(log n).
+
+# For a skewed BST, h = O(n).
+        
+        
+        
+        # IF no recurion then improvement on space complexity
+        # O (1)
+        
+        
         cur = root
         while cur:
             if p.val < cur.val and q.val < cur.val:
