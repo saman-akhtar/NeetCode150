719ac12a03a136201d60c852b41bb5f2a40899e4
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-04-04 08:17:01 -0700

    Time: 0 ms (100.00%) | Memory: 19.8 MB (31.20%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -6,6 +6,34 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def helper(root,low,high):
+            if not root:
+                return True
+            if root.val <= low or root.val >= high:
+                return False
+            return (helper(root.left, low,root.val) and helper(root.right, root.val, high))
+
+
+        return helper(root, float('-inf'), float('inf'))
+
+
+
+
+
+    # WRONG AS it be less that root .. all left shoudl be less that root ..n ot immediate one
+    
+        # if not root :
+        #     return True
+        # if root.right and root.right.val <= root.val :
+        #     return False
+        # if root.left and root.left.val >= root.val:
+        #     return False
+        # return self.isValidBST(root.left) and self.isValidBST(root.right)
+
+                                                                                                                                                                                                                                              
+
+
+
         # if not root:
         #     return True
         # if root.left:
@@ -26,6 +54,21 @@ class Solution:
 
 
 # CORRECT APPROACH
+        def isValid(root, left,right):
+            if not root:
+                return True
+            if not( left < root.val < right):
+                return False
+            return isValid(root.left,left, root.val) and isValid(root.right,root.val, right)
+        return isValid(root, float('-inf'), float('inf'))
+
+
+
+
+
+
+
+
         def isValid( root, left, right):
             if not root:
                 return True
