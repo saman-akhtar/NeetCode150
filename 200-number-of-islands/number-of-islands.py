fa30381e43a437c567e1ebc2399506c14997fa3c
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2025-04-07 08:20:06 -0700

    Time: 318 ms (11.81%) | Memory: 25.6 MB (15.19%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        ROW,COL = len(grid), len(grid[0])
+        visit = set()
+        no_island = 0
+        def dfs(r,c):
+            # no_island += 1 wrong to inc here, as dfs called for each cell it will just inc
+            visit.add((r,c))
+            dir = [[1,0],[0,1],[0,-1],[-1,0]]
+            for r1,c1 in dir:
+                
+                row = r1 + r
+                col = c1 + c
+                if ((row,col) not in visit and row in range(ROW) and col in range(COL) and grid[row][col] == "1") :
+                    dfs(row,col)
+        for r in range(ROW):
+            for c in range(COL):
+                if grid[r][c] == "1" and (r,c) not in visit:
+                    dfs(r,c)
+                    no_island += 1 
+        return no_island
+        
