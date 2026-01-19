37a3e8cb19b1810e632bf678870f25836fe5e8e5
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2026-01-19 10:12:48 -0800

    Time: 548 ms (71.36%) | Memory: 22.2 MB (19.77%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,55 +1,37 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
-        resArr = []
-        # O(n log n)
-        nums = sorted(nums)
         n = len(nums)
-        print("nums:: ",nums)
-        # edge case dupli set ?
-        # O(N)
-        for i in range(n-2):
-            # edge
-            if i > 0 and nums[i] == nums[i-1]:
+        nums.sort()
+        res = []
+        for i in range(n):
+            if i >0 and nums[i] == nums[i-1]:
+                # skip i
                 continue
-            j = i + 1
-            k = n -1
-            # O(n => O(n ^2))
-            while j < k:
-                sums = nums[j] + nums[k] + nums[i]
-                if( sums  == 0):
-                   
-                        resArr.append([nums[i], nums[j], nums[k]])
-                        # # edge even after adding dont stop
-                        j +=1
-                        while j < k and nums[j] == nums[j-1]:
-                            j += 1
-           
-                elif sums > 0:
-                    k -= 1
-                else:
-                    j += 1
-        return resArr
-
-
+            target = -nums[i]
+            l = i + 1
+            r = n - 1
+            while l < r:
             
+                    sums = nums[l] + nums[r]
+                
+                    if sums == target:
+                        arr= [ nums[i], nums[l], nums[r]]
+                        res.append(arr)
+                        l += 1
+                        r -= 1
+                        # imp
+                        # avoid duplicate
+                        while l < r and nums[l]== nums[l-1]:
+                            l +=1
+                        while l < r and nums[r] == nums[r+1]:
+                            r -= 1
 
+                    elif sums > target:
+                        r -=1
+                    else:
+                        l +=1
+  
+        return res
+           
 
-        #TC O (N^2) + O(nlogn)
-        # SC O(k) k unique 
-
-        
-        #approach1
-        # for i in range(len(nums)):
-        #     for j in range(i+1,len(nums)):
-        #         for k in range(j+1,len(nums)):
-                    
-        #             if(nums[i]+ nums[j]+ nums[k] == 0):
-        #                 temp = sorted([nums[i], nums[j], nums[k]])
-        #                 if (temp) not in soln:
-        #                     soln.append(temp)
-        # return soln
-
-# TC N^3 * O(3log3) => O(1)
-# SC O(N)
         
