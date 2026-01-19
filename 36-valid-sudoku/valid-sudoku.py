5fef9ded432824204bef58b58a24e87e62e521b4
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2026-01-18 19:43:21 -0800

    Time: 3 ms (80.94%) | Memory: 19.5 MB (9.58%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,48 +1,47 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        col =defaultdict(set)
-        row =defaultdict(set)
-        sqr =defaultdict(set)
-        for r in range(9):
-            for c in range(9):
-                if board[r][c] == ".":
-                    continue
-                if board[r][c] in row[r] or  board[r][c] in col[c] or  board[r][c] in sqr[(r//3,c//3)] :
-                    
+        COL = len(board[0])
+        ROW = len(board)
+        rowSet = defaultdict(set)
+        colSet = defaultdict(set)
+        boxSet = defaultdict(set)
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] != "." and (board[i][j]  in rowSet[i] or board[i][j]  in colSet[j] or board[i][j]  in boxSet[(i//3,j//3)]):
+                
                     return False
-                row[r].add(board[r][c])
-                col[c].add(board[r][c])
-                sqr[(r//3,c//3)].add(board[r][c])
+                rowSet[i].add(board[i][j])
+                colSet[j].add(board[i][j])
+                boxSet[(i//3,j//3)].add(board[i][j])
         return True
 
 
 
 
+# TC O(9^2)
+# SC O(9^2)
 
 
 
 
 
+        
+        COL = len(board[0])
+        ROW = len(board)
+        rowSet = defaultdict(set)
+        colSet = defaultdict(set)
+        boxSet = defaultdict(set)
 
 
-
-
-
-        # col = defaultdict(set)
-        # row = defaultdict(set)
-        # sqr = defaultdict(set) # (r/c,c/3)
-
-        # for r in range(9):
-
-        #     for c in range(9):
-        #         if(board[r][c]=="."):
-        #             continue
-        #         if(board[r][c] in row[r] or board[r][c] in col[c] or board[r][c] in sqr[(r//3,c//3)] ) :
-        #             return False
-        #         col[c].add(board[r][c])
-        #         row[r].add(board[r][c])
-        #         sqr[(r//3,c//3)].add(board[r][c])
-        # return True
+        for r in range(ROW):
+            for c in range(COL):
+                if (board[r][c] != ".") and (board[r][c] in rowSet[r] or board[r][c] in colSet[c] or board[r][c] in boxSet[(r//3,c//3)]):
+                    return False
+                rowSet[r].add(board[r][c])
+                colSet[c].add(board[r][c])
+                boxSet[(r//3,c//3)].add(board[r][c])
+        return True
 
 # TC O(9^2)
 # SC O(9^2)
