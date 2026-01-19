405a71d8c129afa9896935c369c2396ca724d524
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2026-01-19 08:49:53 -0800

    Time: 10 ms (40.43%) | Memory: 19.7 MB (31.94%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,24 +1,39 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = ''.join([ ch.lower() for ch in s if ch.isalnum()])
-        length = len(s)
-        if length < 2:
-            return True
-        l = 0
-        r = length -1
+#         s = ''.join([ ch.lower() for ch in s if ch.isalnum()])
+#         length = len(s)
+#         if length < 2:
+#             return True
+#         l = 0
+#         r = length -1
       
 
 
-        while l < r:
+#         while l < r:
             
-            if s[l] != s[r]:
-                return False
-            l +=1
-            r -= 1
+#             if s[l] != s[r]:
+#                 return False
+#             l +=1
+#             r -= 1
             
-        return True
-# A man, a plan, a canal: Panama#
+#         return True
+# # A man, a plan, a canal: Panama#
         
 
-        # TC O(N) + O(N)
-        # SC
+#         # TC O(N) + O(N)
+#         # SC O(N) list comprehension
+
+        # OPTIMZED
+        length = len(s)
+        l = 0
+        r = length -1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l +=1
+            while l < r and not s[r].isalnum():
+                r -=1
+            if s[l].lower() != s[r].lower():
+                return False
+            l +=1
+            r -=1
+        return True
