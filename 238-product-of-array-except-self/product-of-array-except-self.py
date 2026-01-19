ac5408dec5bf0850d69e266a1adb320a46992027
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2026-01-18 18:19:45 -0800

    Time: 37 ms (19.23%) | Memory: 27.7 MB (5.77%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,5 +1,75 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+         # question
+         #1 can no be negative 
+       
+       # METHOD 1 : TLE
+        # l = len(nums)
+        # pre =[1] * l
+        # post =[1] * l
+        # res =[1] * l
+        # for i in range(l):
+        #     j = i + 1
+        #     total = 1
+        #     while j < l:
+        #         total *= nums[j]
+        #         j+=1
+        #     pre[i] = total
+
+        # for i in range(l-1,-1,-1):
+        #     j = i -1
+        #     total = 1
+        #     while j > -1:
+        #         total *= nums[j]
+        #         j-=1
+        #     post[i] = total
+        
+
+        # for i in range(l):
+        #     res[i] = pre[i] * post[i]
+        # return res
+            
+
+        # TC O(n^2) + O(n^2) + O(n) = o(n^2)
+        # SC O(n) + O(n)  = O(n)
+
+        # METHOD 2 opmized
+    
+       
+        l = len(nums)
+        res =[1] * l
+        pre =[1] * l
+        post =[1] * l
+#         Prefix product: pref[i] = product of all elements to the left of i
+# potfix product: post[i] = product of all elements to the right of i
+        for i in range(1,l):
+            pre[i] =nums[i-1] * pre[i-1]
+
+        for i in range(l-2,-1,-1):
+            post[i]= nums[i+1] * post[i+1]
+        for i in range(l):
+            res[i] = pre[i] * post[i]
+        return res
+            
+
+        # TC O(n) + O(n) + O(n) = o(n)
+        # SC O(n) + O(n)  = O(n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         # CAN it fit into 32 bit ?
         # can each element be negative positive
