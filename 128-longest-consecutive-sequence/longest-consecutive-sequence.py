c397a05db8fce6592a6f98c5efc170af33f4e163
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2026-01-18 19:18:32 -0800

    Time: 46 ms (72.40%) | Memory: 36.8 MB (19.82%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,104 +1,38 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        # maxl = 0
+        # nset = set(nums)
+        # for n in nums:
+        #     cur_l = 0
+        #     cur_n = n 
+        # here we are check any no 
+        # ie 321  thrice calcuation for lengh
+        # this is repeatd .. how to optimzed?? find when the seq start so count 1
+        #     while (cur_n -1) in nset:
+        #         cur_l+=1
+        #         cur_n -= 1
+            
+        #     maxl = max(maxl,cur_l)
 
-        lseq =0
-        numSet = set(nums)
-        # just loop over set, not list which causes TLE
-        for nums in numSet:
-            streak = 1
-            if nums -1 not in numSet:
-               
-                val = nums +1
-                while val  in numSet:
-                    streak += 1
-                    val += 1
-            lseq = max(lseq, streak)
-        return lseq
+        # return maxl
+                  
+     # TC O(n)[set creation] + O(n^2) (for & while]
 
-
-
-
-
-
-
+     # Sc O(n) set
 
 
+     ## Optimized
+     # Crux find start of seq
+     maxl = 0
+     nset = set(nums)
+     for n in nset:  #vvvv imp dont do for n in nums: reason is if duplicate no then that seq counted multiple times
+        if n -1 not in nset:
+            curl = 1
+            while ( n + curl) in nset:
+                curl += 1
+            maxl = max(curl , maxl)
+     return maxl
 
-
-
-
-
-
-
-
-        # APPProach 2
-        # optimized
-        # check if num -1 doest exist, then calculates longest 
-        # so that u vistit 1 item only ince in ineer loop
-        # longest = 0
-        # numSet = set(nums) # O(N)
-        # for num in numSet:
-        #     streak = 1
-        #     if num -1 not in numSet:
-        #         cur_num = num
-        #         while cur_num + 1 in numSet:
-        #             streak += 1
-        #             cur_num += 1
-        #     longest = max(longest,streak)
-        # return longest
-
-        # TC O(N) + O(n)+ O(N)
-#         Creating the set: O(n)
-# Iterating over the set: O(n)
-# Inner while loops (across all iterations): O(n)
-# SC O(N)
-
-
-
-
-
-
-        # APProach 1 : TLE
-
-        # logic: u loop through each item, if each ietm -1 exist ther is seq
-        # keep incrementing longest
-        # longest = 0
-        # numSet = set(nums) # O(N)
-        # for i in range(len(nums)):
-        #     cur_long = 0
-        #     cur_num = nums[i]
-        #     while cur_num in numSet:
-        #         cur_long += 1
-        #         cur_num -= 1
-        #     longest = max(longest,cur_long)
-        # return longest
-
-        # O(n ^2)
-        # SC O(N)
-
-
-
-
-
-
-
-
-
-
-
-
-        # # o(N)
-        # numSet = set(nums)
-        # longest = 0
-        # # o(N)
-        # for i in nums:
-        #     l = 0
-        #     # lookup is O(1)
-        #     if(i-1) not in numSet:
-        #         while(i + l  in numSet):
-        #             #inner loop O(N)
-        #             l +=1
-        #         longest = max(l, longest)
-        # return longest
-# tc   O(n) + O(n) + O(n) = O(n)
-# sc O(N)
+     # tc O(n) no repeated cal as finding start of seq
+     # sc O(n)
+    
