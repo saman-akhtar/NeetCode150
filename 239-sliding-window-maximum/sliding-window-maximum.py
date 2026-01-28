f5d768980efcf58eceb01e61f12b5be8b1728a5b
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2026-01-28 10:18:10 -0800

    Time: 194 ms (37.44%) | Memory: 35.4 MB (20.98%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,29 +1,58 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        q = deque() # store indices
-        output = []
-        l =r = 0
+
+        # optimized
+        # sliding window
+        # 4 3 2 1.   0 1 2
+        l = 0
+        r = 0
+        q = collections.deque()
+        maxArr = []
         while r < len(nums):
-            # before adding, chck if q is in motonice decre
+            # remove samll                                                                 
             while q and nums[q[-1]] < nums[r]:
                 q.pop()
-
-             
-
-            # add indices
             q.append(r)
 
-            # if out of bound ,remove
+            # remove left value from
             if l > q[0]:
                 q.popleft()
-            
-            # edge case
-            if ( r +1) >= k:
-                output.append(nums[q[0]])
-                l += 1
-            r += 1
-        return output
 
-        # tC O(n)
-        # Sc O(n) beacuse of deque
-        
+            # adding of new
+            if (r+ 1) >= k:
+                maxArr.append(nums[q[0]])
+                l+= 1
+            r += 1
+        return maxArr
+# TC O(N) * O(1)
+# SC
+ # method 2 
+# 4 3 2 1
+
+
+        maxArr = []
+        maxitem = max(nums[:k])
+        maxArr.append(maxitem)
+        l = 0
+
+        for i in range(k,len(nums)):
+            if nums[i] >= maxitem:
+                maxitem = nums[i]
+            else:
+                # remove from left
+                if nums[l] == maxitem:
+                    maxitem = max(nums[l+1: i+1])
+                
+            maxArr.append(maxitem)  
+            l += 1
+        return maxArr
+
+
+        # O(n * (k + k)) = O(n * 2k) = o(nk)
+# 1 2 3 4
+# 3 4
+
+
+
+# 4 4 4 4
+# [-6,-10,-7,-1,-9,9,-8,-4,10,-5,2,9,0,-7,7,4,-2,-10,8,7]
