a08d2a95e262b21df3f18ac3b5ad1f023e291267
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2026-01-28 10:42:18 -0800

    Time: 0 ms (100.00%) | Memory: 19.2 MB (38.56%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,8 +1,22 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        open_brac= ["(",'{', '[']
-        close_brack =[")","}","]"]
+        match = {')': '(', '}': '{', ']': '['}
+        for ch in s:
+            if ch in match:
+                # closing brack
+                if not stack or stack[-1] !=match[ch]:
+                    return False
+                stack.pop()
+            # openini brac:
+            else:
+                stack.append(ch)
+
+        return not stack
+
+
+
+        
         for b in s:
             if stack and stack[-1] in open_brac:
                 prev_brack = stack[-1]
@@ -19,7 +33,7 @@ class Solution:
                         
             else:
                 stack.append(b)
-        print("stack",stack)
+    
         if stack:
             return False
         return True
