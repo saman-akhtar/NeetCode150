764be59ec08594f61e557bb6b6237cdb7e4644ab
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2026-01-29 08:23:05 -0800

    Time: 5 ms (58.80%) | Memory: 23.3 MB (7.20%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -1,38 +1,46 @@
 class MinStack:
- # claryfing : if uniqye item added ?
+
+# invariant: Design stack so that each opertaion 
+# retunr value as if it is actuall a stakc
     def __init__(self):
         self.stack = []
-        self.minStack = []
         
 
     def push(self, val: int) -> None:
-        self.stack.append(val)
-        if self.minStack:
-            min_val = self.minStack[-1]
-        else:
-            min_val = float('inf')
-        self.minStack.append(min(val, min_val))
+        prev= None
+        if self.stack:
+            prev= self.stack[-1]
+        if prev and val >= prev[1]:
+            self.stack.append([val, prev[1]])
+            
+        else:    
+            self.stack.append([val,val])
         
 
     def pop(self) -> None:
+        val = "Stack is empty"
         if self.stack:
-            self.minStack.pop()
-            last = self.stack.pop()
-            return last
-        print("Wrong operation")
+            combined_val = self.stack.pop()
+            val = combined_val[0]
+        return val
         
 
     def top(self) -> int:
+        val = ""
         if self.stack:
-            return self.stack[-1]
-        print("Wrong operation")
+            combined_val =self.stack[-1]
+            val = combined_val[0]
+     
+        return val
 
     def getMin(self) -> int:
+        val = ""
         if self.stack:
-            return self.minStack[-1]
-        
-# TC O(1) for each operation
-# SC Total Space Complexity: O(n)+O(n)=O(n).
+            combined_val =self.stack[-1]
+            val = combined_val[1]
+     
+        return val
+ 
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
