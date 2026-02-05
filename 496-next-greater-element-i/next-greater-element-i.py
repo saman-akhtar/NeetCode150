355426d7b0e732a89e7fad0f18d55e49d403d0a4
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2026-02-04 22:32:29 -0800

    Time: 2 ms (44.84%) | Memory: 19.5 MB (17.66%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,5 +1,31 @@
 class Solution:
-    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:         
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:    
+
+        # MEthod 2 TC O(n) SC O(1)
+
+        # find global next > of all element in num2
+        # create dict 0f  num2
+        nxt_great = {}
+        stack=[]
+        # 4 3 2
+        # 1 2 3 1
+        for n in nums2:
+            while stack and n > stack[-1]:
+                prev = stack.pop()
+                nxt_great[prev]= n
+
+            stack.append(n)
+        print(nxt_great)
+        res = [-1]*len(nums1)
+        for i,n in enumerate(nums1):
+            if n in nxt_great:
+                res[i] = nxt_great[n]
+        return res
+
+
+
+
+        # MEthod 1 no t optimized     
         res = [-1] * len(nums1)
         for i, n1 in enumerate(nums1):
             search_idx = -1
@@ -12,4 +38,5 @@ class Solution:
                         break
 
         return res
-        
+        # TC = O(n²).
+        # SC = O(1)
