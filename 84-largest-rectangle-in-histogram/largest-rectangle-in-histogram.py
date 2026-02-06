79c71f82eb1451986653dc5140f8fae8451feb50
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2026-02-06 10:21:12 -0800

    Time: 104 ms (96.33%) | Memory: 36.5 MB (19.66%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,50 +1,88 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
+
+        # The stack stores increasing bars with their leftmost reach; popping finalizes area.”
+# optimized Method 2
         stack = []
-        maxArea = 0
-        for i, h in enumerate(heights):
+        max_vol = 0
+        n = len(heights)
+        for i,h in enumerate(heights):
             start = i
-            while stack and stack[-1][0] >= h:
-                old_h,old_i = stack.pop()
-                area = old_h * (i -old_i)
-                maxArea = max(maxArea, area)
-                start = old_i
-            stack.append((h,start))
-        for  h, i in stack:
-            area = h * (len(heights) - i)
-            maxArea = max(maxArea, area)
-        return maxArea
-
-
-
-
-
-
-        # TC O(N)
-        # SC O(N)
-
-
-        # COmplute maxleft & maxright
-
-    #     maxA = 0
-
-    #     for i in range(len(heights)):
-    #         h = heights[i]
-    #         rightMost = i + 1
-    #         while rightMost < len(heights) and heights[rightMost] >= heights[i]:
-    #             rightMost += 1
-    #         leftMost = i -1 
-    #         while leftMost >=0 and heights[leftMost] >= heights[i]:
-    #             leftMost -= 1
-
-    #         maxA = max(maxA, heights[i] * (rightMost - leftMost -1))
-    #     return maxA
-
-    # # TC O(n^2)
-
-    # # SC O(1)
+            while stack and stack[-1][1] > h:
+                idx, prev = stack.pop()
+                max_vol =max(max_vol, prev * (i - idx))
+                start = idx
             
+            stack.append((start,h))
+        for i,h in stack:
+            
+            max_vol = max(max_vol, h * (n - i))  
+        return max_vol
+
+       # TC O(N)
+       # SC O(N)
 
 
 
-        
+        # metho1 1 two pad
+        stack = []
+        max_area = 0
+        n = len(heights)
+        left =[0 ]*n
+        for i in range(n):
+            h = heights[i]
+            while stack and stack[-1] >= h:
+
+                    old_h  = stack.pop()
+                    left[i] +=1
+            stack.append((h))
+        stack = []
+        right =[0 ]*n
+        for i in range(n-1,-1,-1):
+            h = heights[i]
+            
+            while stack and stack[-1] >= h:
+
+                    old_h  = stack.pop()
+                    right[i] +=1
+            stack.append((h))
+        print("left",left)   
+        print("right",right)
+        for i in range(n):
+            max_area = max(max_area, heights[i]* (left[i]+right[i]+1))
+        return max_area
+
+
+
+
+
+        #     while stack and stack[-1] >= h:
+
+        #             idx , old_h = stack.pop()
+        #             max_area =max(max_area, h * ( i - idx))
+        #             start = idx
+        #     stack.append((start,h))
+
+        # stack = []
+        # max_area = 0
+        # n = len(heights)
+        # for i in range(n):
+        #     h = heights[i]
+        #     start = i
+        #     # left side, pop >=
+        #     while stack and stack[-1][1] > h:
+
+        #             idx , old_h = stack.pop()
+        #             max_area =max(max_area, old_h * ( i - idx))
+        #             start = idx
+        #     stack.append((start,h))
+        # # clean up rem
+        # for i, h in stack:
+        #     max_area = max(max_area, h * (n -i))
+
+
+        # return max_area
+
+#TC O(N^2)
+# SC O(N)
+     
