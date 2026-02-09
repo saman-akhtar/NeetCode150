1653c09432c0d77b86b887d21e883e139e605dec
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2026-02-09 09:44:41 -0800

    Time: 164 ms (60.13%) | Memory: 20.5 MB (59.60%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,52 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+       
+
+        # 1 2 3 4 5 
+        # hr      h lenght
+        # 5   5  same -max
+        # 3   5  <.  - max
+        #        >
+        #     1
+
+        # given third condition
+        # i.e h > piles lenght
+
+        #brute force -- 1 - max(op)
+        # O(max(p) * p[len of pile])
+
+
+        # Binary searc
+        # TC O(log(max(p)) * p)
+        import math
+        maxb = max(piles)
+        min_res = maxb
+        l = 1
+        r = maxb
+
+        def time_consumed(mid):
+            tc =0
+            for n in piles:
+                tc +=   math.ceil( n /mid)
+            return tc
+
+
+        while l <= r:
+         
+            mid = (l +r)//2
+            tc = time_consumed(mid)
+           
+            if  tc <= h:
+                res = mid
+                min_res = min(mid, min_res) #
+
+                r = mid - 1 # # try smaller
+            else:
+                # tc > h:
+                l = mid + 1# need bigger speed
+
+           
+        
+        return min_res
+
+            
