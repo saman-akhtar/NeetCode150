52181863d4f68f0eafff33a9d3287ac6c8d12e3a
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2026-02-09 10:26:54 -0800

    Time: 0 ms (100.00%) | Memory: 19.8 MB (8.29%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,70 +1,25 @@
 class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool: 
-
-        rows = len(matrix)
-        cols = len(matrix[0])
-        top = 0
-        bot = rows - 1
-
-        # Binary search to find the correct row
-        while top <= bot:
-            mid = (top + bot) // 2
-            if target > matrix[mid][-1]:
-                top = mid + 1
-            elif target < matrix[mid][0]:
-                bot = mid - 1
-            else:
-                break
-        
-        # If no valid row was found
-        if not (top <= bot):
-            return False
-
-        # Binary search within the identified row
-        row = (top + bot) // 2
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        r = len(matrix)
+        c = len(matrix[0])
         l = 0
-        r = cols - 1
-        while l <= r:
-            m = (l + r) // 2
-            if matrix[row][m] == target:
+        h = c-1
+        r1 =0
+        for i in range(r):
+            if target <= matrix[i][c-1]:
+                r1 = i
+                break
+        print("r1",r1)
+        while l <= h:
+            mid = (l + h)//2
+            print(l, h, mid)
+            if matrix[r1][mid]== target:
                 return True
-            elif matrix[row][m] < target:
-                l = m + 1
+            elif matrix[r1][mid] > target:
+                   h = mid - 1
+                
             else:
-                r = m - 1
-        
+                l = mid +1
+             
         return False
 
-   # Time Complexity: 
-# \U0001d442
-# (
-# log
-# ⁡
-# \U0001d45a
-# +
-# log
-# ⁡
-# \U0001d45b
-# )
-# =
-# \U0001d442
-# (
-# log
-# ⁡
-# (
-# \U0001d45a
-# ×
-# \U0001d45b
-# )
-# )
-# O(logm+logn)=O(log(m×n)), where 
-# \U0001d45a
-# m is the number of rows and 
-# \U0001d45b
-# n is the number of columns.
-# Space Complexity: 
-# \U0001d442
-# (
-# 1
-# )
-# O(1), as we are using a constant amount of space.
