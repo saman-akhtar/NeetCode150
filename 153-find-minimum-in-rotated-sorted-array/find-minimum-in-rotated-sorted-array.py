8c0afbcadbfe10507875407078a82be5db798067
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2026-02-10 10:02:27 -0800

    Time: 0 ms (100.00%) | Memory: 19.5 MB (41.08%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -5,8 +5,7 @@ class Solution:
         n = len(nums)
         r = n-1
         while l <= r:
-            print("li ",l,r)
-            print("val",nums[l],nums[r])
+       
             mid = (l + r)//2
             min_res= min(min_res,nums[mid])
             left_r = (nums[l], nums[mid-1])
@@ -18,14 +17,15 @@ class Solution:
             right_r = (nums[l1], nums[r])
 
             if left_r[0] < right_r[1] or left_r[1] < right_r[1] :
-            # if (right_r[0] < left_r[1] or right_r[1] < left_r[0]) and ( right_r[1] > left_r[1]):
+        
                 r = mid -1
             else:
                 l = mid +1
         return min_res
 
 
-        
+   # TC O(logN)   
+   # SC O(n)  
 
     # 7 8  9   10 11  13. 14. 15 0 1 2 3 4 5 6
 
