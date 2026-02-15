ded9712d240bcbaabf4568589053b9489225b491
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2026-02-15 15:31:24 -0800

    Time: 3 ms (49.01%) | Memory: 19.8 MB (11.74%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,5 +1,51 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:  
+        A, B = nums1, nums2
+        if len(A) > len(B):
+            A, B = B, A
+
+        n = len(A)
+        m = len(B)
+
+        total = n + m
+        half = total // 2
+        even = (total % 2 == 0)
+
+        l = -1
+        r = n - 1
+
+        while True:
+            i = (l + r) // 2
+            j = half - i - 2
+
+            Aleft  = A[i]   if i >= 0 else float("-inf")
+            Aright = A[i+1] if i+1 < n else float("inf")
+            Bleft  = B[j]   if j >= 0 else float("-inf")
+            Bright = B[j+1] if j+1 < m else float("inf")
+
+            if Aleft <= Bright and Bleft <= Aright:
+                if even:
+                    return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+                else:
+                    return min(Aright, Bright)
+
+            elif Aleft > Bright:
+                r = i - 1
+            else:
+                l = i + 1
+
+            
+
+
+                
+
+
+
+
+
+
+
+
         n = len(nums1)
         m = len(nums2)
         is_even = True if (n + m ) % 2== 0 else False
