0a64120bfee9d95b5bd7ade74d74661f75b1740b
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2026-02-17 07:44:46 -0800

    Time: 0 ms (100.00%) | Memory: 19.5 MB (19.68%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -20,7 +20,7 @@ class Solution:
         while count > 0 and cur:
             cur = cur.next
             count -= 1
-        # c = 0
+
         if cur.next:
                 new_element = cur.next.next
                 cur.next = new_element
@@ -34,96 +34,5 @@ class Solution:
         
 
 
-
-        # def reverseList(head:ListNode):
-        #     prev = None
-        #     curr = head
-
-        #     while curr:
-        #         nxt_node = curr.next
-        #         curr.next = prev
-        #         prev = curr
-
-        #         curr = nxt_node
-            
-        #     return prev
-
-        # def remove(head: ListNode,n):
-
-        #     head = reverseList(head)
-
-        #     if n == 1:
-        #         head = head.next
-            
-        #     else:
-        #         curr = head
-
-        #         for i in range(n - 2):
-        #             curr = curr.next
-
-        #         curr.next = curr.next.next
-
-
-        #     head = reverseList(head)
-        #     return head
-
-        # return remove(head, n)
-
-
-
-
-    #     length = 0
-    #  #   print("head", head)
-    #     curr = head
-
-    #     while curr:
-            
-    #         length += 1
-            
-    #         curr = curr.next
-    #    #     print("len", length)
-    #    #     print("initial curr",curr)
-            
-            
-        
-    #     pos_begin = length - n 
-
-    #     if pos_begin == 0:
-    #         return head.next
-
-    #     curr = head
-
-    #  #   print("current", curr)
-
-    #     for i in range(pos_begin - 1):
-    #         curr = curr.next
-
-    #      #   print("inside curr", curr)
-
-    #     curr.next = curr.next.next
-
-
-    #  #   print( "head", head)
-
-    #     return head
-
-
-
-
-    #     # dummy = ListNode(0, head)
-
-    #     # l = dummy
-    #     # r = dummy
-
-    #     # for _ in range(n):
-    #     #     r = r.next
-
-    #     # while r.next:
-    #     #     r = r.next
-    #     #     l = l.next
-
-    #     # l.next = l.next.next
-
-    #     # return dummy.next
 
         
