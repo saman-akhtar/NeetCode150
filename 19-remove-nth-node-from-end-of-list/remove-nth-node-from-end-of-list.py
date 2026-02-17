8058f76312227e5e167285ebd5a451e606391411
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2026-02-17 07:44:20 -0800

    Time: 0 ms (100.00%) | Memory: 19.4 MB (19.68%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -5,37 +5,125 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        dummy = ListNode(0, head)
-        slow = dummy
-        fast = head
-        while n > 0 and fast:
-            fast = fast.next
-            n -= 1
-        while fast:
-            slow = slow.next
-            fast = fast.next
-        slow.next = slow.next.next
-        return dummy.next
+        p = head
+        l = 0
+        c =0
+        while p:
+            p = p.next
+            l += 1
+        if n > l:
+            return head 
+        count = l - n -1
+        cur = head
+        if count < 0:
+            return head.next
+        while count > 0 and cur:
+            cur = cur.next
+            count -= 1
+        # c = 0
+        if cur.next:
+                new_element = cur.next.next
+                cur.next = new_element
+         
+        return head
+
+ ## TC O(N)
+ # SC O(`1`)
 
         
-        # Approch 2
-#         d= cur = head
-#         l = 0
-#         while cur:
-#             cur = cur.next
-#             l += 1
-#         r = l - n
-#         if r == 0:
-#             return head.next
-#         while r != 0:
+        
+
+
+
+        # def reverseList(head:ListNode):
+        #     prev = None
+        #     curr = head
+
+        #     while curr:
+        #         nxt_node = curr.next
+        #         curr.next = prev
+        #         prev = curr
+
+        #         curr = nxt_node
             
-#             r -=1
-#             if( r == 0):
-#                 d.next = d.next.next
-#             else:
-#                 d = d.next
+        #     return prev
 
-#         return head
+        # def remove(head: ListNode,n):
 
-# # TC O(N) = O(N) + O(N)
-# # SC O(1)    
+        #     head = reverseList(head)
+
+        #     if n == 1:
+        #         head = head.next
+            
+        #     else:
+        #         curr = head
+
+        #         for i in range(n - 2):
+        #             curr = curr.next
+
+        #         curr.next = curr.next.next
+
+
+        #     head = reverseList(head)
+        #     return head
+
+        # return remove(head, n)
+
+
+
+
+    #     length = 0
+    #  #   print("head", head)
+    #     curr = head
+
+    #     while curr:
+            
+    #         length += 1
+            
+    #         curr = curr.next
+    #    #     print("len", length)
+    #    #     print("initial curr",curr)
+            
+            
+        
+    #     pos_begin = length - n 
+
+    #     if pos_begin == 0:
+    #         return head.next
+
+    #     curr = head
+
+    #  #   print("current", curr)
+
+    #     for i in range(pos_begin - 1):
+    #         curr = curr.next
+
+    #      #   print("inside curr", curr)
+
+    #     curr.next = curr.next.next
+
+
+    #  #   print( "head", head)
+
+    #     return head
+
+
+
+
+    #     # dummy = ListNode(0, head)
+
+    #     # l = dummy
+    #     # r = dummy
+
+    #     # for _ in range(n):
+    #     #     r = r.next
+
+    #     # while r.next:
+    #     #     r = r.next
+    #     #     l = l.next
+
+    #     # l.next = l.next.next
+
+    #     # return dummy.next
+
+        
