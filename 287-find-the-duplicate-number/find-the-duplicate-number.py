013f63ddba36603a42101fb930424065484fab3f
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2026-02-19 10:59:33 -0800

    Time: 25 ms (65.08%) | Memory: 33.5 MB (54.03%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,16 +1,18 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        map1 = {}
-        for i in range(len(nums)):
-            map1[nums[i]] =  map1.get(nums[i], 0) + 1
-            if map1[nums[i]] > 1:
-                return nums[i]
-        #     cur = cur.next
+        slow = 0
+        fast =0
 
-
-        # while cur:
-        #     map1[cur.val] = map1.get(cur.val, 0) + 1
-        #     if map1[cur.val] > 1:
-        #         return cur.val
-        #     cur = cur.next
-        
+        # find start if a cycke
+        while True:
+            slow = nums[slow] # travek 1 at a time
+            fast = nums[nums[fast]] # go twice
+            if slow == fast:
+                break 
+       # if two pointer pont to start of a cycle, tahts the answer
+        slow2 = 0 # start from start
+        while True:
+            slow = nums[slow] # withing cycke , sl slow2& slwo are eqidistance for 
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
