b6bc30c54b70848e8d9c3f351f620b82e6f1cc6d
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2026-02-19 06:47:27 -0800

    Time: 13 ms (7.77%) | Memory: 19.6 MB (9.15%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -4,27 +4,25 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        if not l1 and not l2:
-            return l1
-        dummy = cur = ListNode()
-        carry = 0
-        while l1 or l2:
-            val1 = l1.val if l1 else 0
-            val2 = l2.val if l2 else 0
-            val = val1 + val2 + carry
-            carry = val // 10
-            val = val % 10
-            cur.next = ListNode(val)
-            cur = cur.next
-            if (l1):
-                l1 = l1.next 
-            if l2:
-                l2 = l2.next 
-
-        if carry == 1:
-            cur.next = ListNode(1)
-        return dummy.next
-
-# TC O(n + m)
-# SC O(1)
+    def addTwoNumbers(self, l1, l2):
+        dummy = ListNode(0)
+        copy = dummy
+        c = 0
+        while l1 or l2 or c:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            total = v1 + v2 + c
+            c = total // 10
+            print(total , total % 10)
+            dummy.val = total % 10
+            if l1:
+                l1 = l1.next
+            if l2 :
+                l2 = l2.next
+            if c or l1 or l2:
+                newNode = ListNode(0)
+                dummy.next = newNode
+                dummy= newNode
+        return copy
+        # Time → O(max(n, m))
+        # Space → O(max(n, m))    
