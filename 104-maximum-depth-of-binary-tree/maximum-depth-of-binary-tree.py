12abefd82c1637ec6827ffc49abd238e588e6d15
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2026-03-03 10:33:46 -0800

    Time: 0 ms (100.00%) | Memory: 20.3 MB (54.41%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -8,37 +8,7 @@ class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        left = 1 +  self.maxDepth(root.left)
-        right = 1 +  self.maxDepth(root.right)
-        return max(left,right)
-
-
-
-# TC O(N)
-# SC best case O(logN)
-#    worst case O(N)
-
-
-
-
-
-
-
-    
-
-        def findHeight(root, height):
-            if not root:
-                return 0
-
-            # if (root.left):
-            #     findHeight(root.left, height)
-            # if (root.right):
-            #     findHeight(root.right, height)
-            height = 1 + max(findHeight(root.right, height), findHeight(root.left, height))
-            return height
-        return findHeight(root, 0)
-
-# tc : O(N)
-# Sc: height -> balanced tree O(log n)
-#.           -> degerated tree : sort of limnked list -> total no of node O(N)
+        lh = self.maxDepth(root.left)
+        rh = self.maxDepth(root.right)
+        return 1 + max(lh,rh)
         
