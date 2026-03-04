459e1074e64d4ceadb54f23e1b3971b200407916
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2026-03-04 11:07:02 -0800

    Time: 43 ms (43.29%) | Memory: 20.5 MB (23.29%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -6,26 +6,24 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        def isSameTree(root,subRoot):
+        def isSame(root, subRoot):
             if not root and not subRoot:
                 return True
-            if root and subRoot and root.val == subRoot.val:
-          
-                return isSameTree(root.left,subRoot.left) and isSameTree(root.right,subRoot.right)
-            return False
-
-        if  not subRoot:
-            return True
+            if not root or not subRoot:
+                return False
+            #if matc
+            if root.val != subRoot.val:
+                return False
+            
+            return isSame(root.left,subRoot.left) and isSame(root.right,subRoot.right)
+        # not match
         if not root:
             return False
-        isSameTree = isSameTree(root,subRoot)
-            
-        if (isSameTree):
+        if isSame(root,subRoot):
             return True
+            # if root & subroot not same. May be root children can be be same 
+            # so call isSUbtree func next
+        return self.isSubtree(root.left,subRoot) or self.isSubtree(root.right,subRoot)
 
-        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right,subRoot)
-
-        
-#         Metric	Complexity
-# Time Complexity	O(n × m)
-# Space Complexity	O(max(n, m))
+        # TC O(N *M) - for every root search for ery subrooot
+        # SC O(H)
