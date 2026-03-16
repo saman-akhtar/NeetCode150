f7adb36ddbb33772312d4904bdd84e40a02b63d0
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2026-03-16 08:56:11 -0700

    Time: 0 ms (100.00%) | Memory: 19.2 MB (88.31%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -6,42 +6,19 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
-            return []
-        res = []
         q = deque([root])
+        res = []
         while q:
-            n = len(q)
-            cur_res = []
-            for i in range(n):
+            r = None
+            for i in range(len(q)):
                 node = q.popleft()
+                
                 if node:
-                    cur_res.append(node.val)
-                    q.append(node.left)
+                    if r is  None:
+                        r = node.val
+                    
                     q.append(node.right)
-            if cur_res:
-                res.append(cur_res[-1])
-        return res
-
-# TC ON
-# SC ON
-
-        #approach 1
-    # def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-    #     q = deque([root])
-    #     res = []
-    #     while q:
-    #         qlen = len(q)
-    #         for i in range(qlen):
-    #             node = q.popleft()
-    #             if node:
-    #                 if node.right:
-    #                     q.append(node.right)
-    #                 if node.left:
-    #                     q.append(node.left)
-    #                 if i  == 0:
-    #                     res.append(node.val)
-    #     return res
-# tc ON
-# Sc O N
-#n the worst case (for a balanced binary tree), the maximum number of nodes in the queue is the width of the tree, which occurs at the largest level of the tree.
+                    q.append(node.left)
+            if r is not None:
+                res.append(r)
+        return res 
