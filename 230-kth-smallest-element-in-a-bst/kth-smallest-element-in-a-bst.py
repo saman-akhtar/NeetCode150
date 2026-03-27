670db927f812874a4de5b2594d3b7c3648839c99
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2026-03-27 08:43:53 -0700

    Time: 0 ms (100.00%) | Memory: 22.3 MB (50.18%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -6,86 +6,18 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        # treat each round in func as treating1 node
-        # NOTE this is bst so utilize already ordered form
-        count = 0
-        res = None
-        def inorder(root):
-            nonlocal count, res
+        res= []
+        def dfs(root):
+            nonlocal res
             if not root:
-                return 
-            inorder(root.left)
-            # now u r tretaing cur note
-            count += 1
-            if (count == k):
-                res = root.val
-            inorder(root.right)
-            return
+                return None
+            dfs(root.left)
+            if len(res)== k:
+                return res
+            res.append(root.val)
+            if len(res)== k:
+                return res
+            dfs(root.right)
+        dfs(root)
+        return res[-1]
         
-
-        inorder(root)
-        return res
-
-
-
-
-
-
-       
-        # TC O(N)
-        # SC is O(h) in saving stack space
-
-
-        # approach 1
-        # res = []
-        # def findSmall(root):
-        #     if not root:
-        #         return
-        #     findSmall(root.left)
-        #     res.append(root.val)
-        #     findSmall(root.right)
-        # findSmall(root)
-        # return res[k-1]
-        # tC O(N)
-        # SC O(N) + O(h) => storing all node + call stack
-
-
-
-
-
-        #approach 3
-
-        arr = []
-        cur = root
-        stack = []
-        n = 0
-        while cur or stack:
-            while cur:
-                stack.append(cur)
-                cur = cur.left
-            cur = stack.pop()
-            n += 1
-            if (n == k):
-                return cur.val
-            
-            cur = cur.right
-    # TC O(N)
-    # SC O(n)
-        
-
-
-
-        # recursive approach
-        # arr = []
-        # def findSmall(root):
-        #     if not root:
-        #         return 
-        #     findSmall(root.left)
-        #     arr.append(root.val)
-            
-        #     findSmall(root.right)
-        # findSmall(root)
-        # return arr[k-1]
-# TC O(N)
-# SC O(N) 
-
