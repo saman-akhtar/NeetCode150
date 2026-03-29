62e4049e181704c49244810f6d8dfe45f0260932
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2026-03-28 20:34:40 -0700

    Time: 88 ms (63.32%) | Memory: 22.7 MB (52.65%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
@@ -13,64 +13,60 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        res = []
-        def dfs(root):
-            nonlocal res
-            if not root:
-                res.append("N")
-                return 
-            res.append(str(root.val))
-            dfs(root.left)
-            
-            dfs(root.right)
-            return
-        dfs(root)
-        ans = ",".join(res)
-        print("ANS",ans)
-        return ans
-        # n 2 n 1 3 4 5 
-        # 1 2 n n 3 4 nul null 5 null nill
-         # TC O(N)
-         # SC : stack , res => O(h)
-         # WORST CASE O(N)
-         # best case O(logN)
+
+        arr = []
+        if not root:
+            return ""
+        q = deque([root])
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node:
+                    arr.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+                else:
+                    arr.append("null")
+        print("Arr",arr)
+
+        return ",".join(map(str, arr))
+                
+
+
+        
+
     def deserialize(self, data):
         """Decodes your encoded data to tree.
         
         :type data: str
         :rtype: TreeNode
         """
-        res = data.split(",")
-        self.i = 0
-        # 1 2 n n 3 4 nul null 5 null nill
-        def createTree():
-            # base case
-            if res[self.i] == "N":
-                # also inc i so that we tell process nxt
-                self.i += 1
-                return None
-            ch = res[self.i]
-            node = TreeNode(int(ch))
-            self.i += 1
-            node.left = createTree()
-            node.right = createTree()
-            return node
-        node = createTree()
-        return node
-
-
-
-
-
-
-
-            
-# Time complexity: 
-
-# O(n)
-# Space complexity: 
-
-# O(n)     
+        if not data:
+            return None
+        i = 1# start from second element
+        arr = data.split(",")
+        root = TreeNode(int(arr[0]))
+        q = deque([root])
+        n = len(arr)
+        while q:
+            node = q.popleft()
+            if i < n:
+                if arr[i] != "null":
+                    new_node= TreeNode(int(arr[i]))
+                    node.left = new_node
+                    q.append(node.left)
+                i += 1
+            if i < n:
+                if arr[i] != "null":
+                    new_node= TreeNode(int(arr[i]))
+                    node.right = new_node
+                    q.append(node.right)
+                i += 1
+        
+        
+        return root
+        
+        
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
