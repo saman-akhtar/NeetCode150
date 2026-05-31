2a9c72160227fa155049c60f46d9d30e5f2e6130
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2026-05-31 09:57:48 -0700

    Time: 43 ms (85.45%) | Memory: 36.7 MB (38.36%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,38 +1,15 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        # maxl = 0
-        # nset = set(nums)
-        # for n in nums:
-        #     cur_l = 0
-        #     cur_n = n 
-        # here we are check any no 
-        # ie 321  thrice calcuation for lengh
-        # this is repeatd .. how to optimzed?? find when the seq start so count 1
-        #     while (cur_n -1) in nset:
-        #         cur_l+=1
-        #         cur_n -= 1
-            
-        #     maxl = max(maxl,cur_l)
+        count = 0
+        nSet = set(nums)
+        for item in nSet:
+            if (item + 1) not in nSet:
+                #start of 
+                cur_count = 1
+                newval = (item - 1)
+                while  newval in nSet:
+                    cur_count +=1
+                    newval -= 1
+                count = max(cur_count, count)
 
-        # return maxl
-                  
-     # TC O(n)[set creation] + O(n^2) (for & while]
-
-     # Sc O(n) set
-
-
-     ## Optimized
-     # Crux find start of seq
-     maxl = 0
-     nset = set(nums)
-     for n in nset:  #vvvv imp dont do for n in nums: reason is if duplicate no then that seq counted multiple times
-        if n -1 not in nset:
-            curl = 1
-            while ( n + curl) in nset:
-                curl += 1
-            maxl = max(curl , maxl)
-     return maxl
-
-     # tc O(n) no repeated cal as finding start of seq
-     # sc O(n)
-    
+        return count
